b517b7c25988b74445f0cc1dab215e4c0174cbbc
Simon Schmid <mail@sschmid.com>
2014-03-08 00:59:25 +0100

    Empty project with nspec

---
+++ Tests/Tests/describe_Tests.cs
@@ -0,0 +1,12 @@
+﻿using NSpec;
+
+public class describe_Tests : nspec {
+    void it_works() {
+        true.should_be_true();
+    }
+
+    void it_fails() {
+        true.should_be_false();
+    }
+}
+
