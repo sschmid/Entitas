89cff3afceb3a0db6175ef08b1a431eb0459539f
Simon Schmid <mail@sschmid.com>
2014-03-10 14:13:04 +0100

    Added Entity

---
+++ Entitas/Entitas/Entity.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entitas {
+    public class Entity {
+        public event EntityChange OnComponentAdded;
+        public event EntityChange OnComponentRemoved;
+        public event EntityChange OnComponentReplaced;
+
+        public delegate void EntityChange(Entity entity, IComponent component);
+
+        public ulong creationIndex { get { return _creationIndex; } }
+
+        readonly Dictionary<Type, IComponent> _components = new Dictionary<Type, IComponent>();
+        readonly ulong _creationIndex;
+
+        public Entity() {
+            _creationIndex = 0;
+        }
+
+        public Entity(ulong creationIndex) {
+            _creationIndex = creationIndex;
+        }
+
+        public void AddComponent(IComponent component) {
+            var type = component.GetType();
+            if (HasComponent(type))
+                throw new EntityAlreadyHasComponentException(string.Format("Cannot add {0} to {1}.", type, this), type);
+
+            _components.Add(type, component);
+            if (OnComponentAdded != null)
+                OnComponentAdded(this, component);
+        }
+
+        public void RemoveComponent(Type type) {
+            if (!HasComponent(type))
+                throw new EntityDoesNotHaveComponentException(string.Format("Cannot remove {0} from {1}.", type, this), type);
+
+            var component = _components[type];
+            _components.Remove(type);
+            if (OnComponentRemoved != null)
+                OnComponentRemoved(this, component);
+        }
+
+        public void ReplaceComponent(IComponent component) {
+            var type = component.GetType();
+            if (HasComponent(type))
+                _components.Remove(type);
+            _components.Add(type, component);
+            if (OnComponentReplaced != null)
+                OnComponentReplaced(this, component);
+        }
+
+        public IComponent GetComponent(Type type) {
+            if (!HasComponent(type))
+                throw new EntityDoesNotHaveComponentException(string.Format("Cannot get {0} from {1}.", type, this), type);
+
+            return _components[type];
+        }
+
+        public bool HasComponent(IComponent component) {
+            return _components.ContainsValue(component);
+        }
+
+        public bool HasComponent(Type type) {
+            return _components.ContainsKey(type);
+        }
+
+        public bool HasComponents(IEnumerable<Type> types) {
+            foreach (var type in types)
+                if (!_components.ContainsKey(type))
+                    return false;
+
+            return true;
+        }
+
+        public bool HasAnyComponent(IEnumerable<Type> types) {
+            foreach (var type in types)
+                if (_components.ContainsKey(type))
+                    return true;
+
+            return false;
+        }
+
+        public HashSet<IComponent> GetComponents() {
+            return new HashSet<IComponent>(_components.Values);
+        }
+
+        public HashSet<Type> GetComponentTypes() {
+            return new HashSet<Type>(_components.Keys);
+        }
+
+        public void RemoveAllComponents() {
+            var types = GetComponentTypes();
+            foreach (var type in types)
+                RemoveComponent(type);
+        }
+
+        public override string ToString() {
+            var componentsStr = string.Empty;
+            var components = GetComponents();
+            const string seperator = ", ";
+            foreach (var component in components)
+                componentsStr += component + seperator;
+
+            if (componentsStr != string.Empty)
+                componentsStr = componentsStr.Substring(0, componentsStr.Length - seperator.Length);
+
+            return string.Format("Entity(" + componentsStr + ")");
+        }
+    }
+
+    public class EntityAlreadyHasComponentException : Exception {
+        public EntityAlreadyHasComponentException(string message, Type type) :
+            base(message + "\nEntity already has a component of type " + type) {
+        }
+    }
+
+    public class EntityDoesNotHaveComponentException : Exception {
+        public EntityDoesNotHaveComponentException(string message, Type type) :
+            base(message + "\nEntity does not have a component of type " + type) {
+        }
+    }
+}
+
---
+++ Tests/Tests/describe_Entity.cs
@@ -0,0 +1,212 @@
+﻿using Entitas;
+using NSpec;
+
+class describe_Entity : nspec {
+    Entity _e;
+    IComponent _componentA;
+    IComponent _componentB;
+
+    void fail() {
+        true.should_be_false();
+    }
+
+    void before_each() {
+        _e = new Entity();
+        _componentA = new ComponentA();
+        _componentB = new ComponentB();
+    }
+
+    void it_has_specified_creation_index() {
+        const int i = 1;
+        new Entity(i).creationIndex.should_be(i);
+    }
+
+    void when_entity_created() {
+        it["has default creation index"] = () => {
+            _e.creationIndex.should_be(0);
+        };
+
+        it["doesn't have component which hasn't been added"] = () => {
+            _e.HasComponent(_componentA).should_be_false();
+        };
+
+        it["has added component"] = () => {
+            _e.AddComponent(_componentA);
+            _e.HasComponent(_componentA).should_be_true();
+        };
+
+        it["has component of type when component of that type was added"] = () => {
+            _e.AddComponent(_componentA);
+            _e.HasComponent(typeof(ComponentA)).should_be_true();
+        };
+
+        it["doesn't have component of type when no component of that type was added"] = () => {
+            _e.HasComponent(typeof(ComponentA)).should_be_false();
+        };
+
+        it["doesn't have components of types when no components of these types were added"] = () => {
+            _e.HasComponents(new [] { typeof(ComponentA) }).should_be_false();
+        };
+
+        it["doesn't have components of types when not all components of these types were added"] = () => {
+            _e.AddComponent(_componentA);
+            _e.HasComponents(new [] { typeof(ComponentA), typeof(ComponentB) }).should_be_false();
+        };
+
+        it["has components of types when all components of these types were added"] = () => {
+            _e.AddComponent(_componentA);
+            _e.AddComponent(_componentB);
+            _e.HasComponents(new [] { typeof(ComponentA), typeof(ComponentB) }).should_be_true();
+        };
+
+        it["doesn't have any components of types when no components of these types were added"] = () => {
+            _e.HasAnyComponent(new [] { typeof(ComponentA) }).should_be_false();
+        };
+
+        it["has any components of types when any component of these types was added"] = () => {
+            _e.AddComponent(_componentA);
+            _e.HasAnyComponent(new [] { typeof(ComponentA), typeof(ComponentB) }).should_be_true();
+        };
+
+        it["removes a component of type"] = () => {
+            _e.AddComponent(_componentA);
+            _e.RemoveComponent(typeof(ComponentA));
+            _e.HasComponent(_componentA).should_be_false();
+        };
+
+        it["gets a component of type"] = () => {
+            _e.AddComponent(_componentA);
+            _e.GetComponent(typeof(ComponentA)).should_be_same(_componentA);
+        };
+    
+        it["replaces existing component"] = () => {
+            _e.AddComponent(_componentA);
+            var newComponentA = new ComponentA();
+            _e.ReplaceComponent(newComponentA);
+            _e.GetComponent(typeof(ComponentA)).should_be_same(newComponentA);
+        };
+
+        it["replacing a non existing component just adds component"] = () => {
+            var newComponentA = new ComponentA();
+            _e.ReplaceComponent(newComponentA);
+            _e.GetComponent(typeof(ComponentA)).should_be_same(newComponentA);
+        };
+
+        it["gets empty hashSet of components when no components were added"] = () => {
+            _e.GetComponents().should_be_empty();
+        };
+
+        it["gets empty hashSet of component types when no components were added"] = () => {
+            _e.GetComponentTypes().should_be_empty();
+        };
+
+        it["gets all components"] = () => {
+            _e.AddComponent(_componentA);
+            _e.AddComponent(_componentB);
+            var allComponents = _e.GetComponents();
+            allComponents.should_contain(_componentA);
+            allComponents.should_contain(_componentB);
+        };
+
+        it["gets all component types"] = () => {
+            _e.AddComponent(_componentA);
+            _e.AddComponent(_componentB);
+            var allComponentTypes = _e.GetComponentTypes();
+            allComponentTypes.should_contain(typeof(ComponentA));
+            allComponentTypes.should_contain(typeof(ComponentB));
+        };
+
+        it["removes all components"] = () => {
+            _e.AddComponent(_componentA);
+            _e.AddComponent(_componentB);
+            _e.RemoveAllComponents();
+            _e.HasComponent(typeof(ComponentA)).should_be_false();
+            _e.HasComponent(typeof(ComponentB)).should_be_false();
+            _e.GetComponents().should_be_empty();
+            _e.GetComponentTypes().should_be_empty();
+        };
+
+        it["can ToString"] = () => {
+            _e.AddComponent(_componentA);
+            _e.AddComponent(_componentB);
+            _e.ToString().should_be("Entity(ComponentA, ComponentB)");
+        };
+
+        context["events"] = () => {
+            it["dispatches OnComponentAdded when adding a component"] = () => {
+                Entity eventEntity = null;
+                IComponent eventComponent = null;
+                _e.OnComponentAdded += (entity, component) => {
+                    eventEntity = entity;
+                    eventComponent = component;
+                };
+                _e.AddComponent(_componentA);
+                eventEntity.should_be_same(_e);
+                eventComponent.should_be_same(_componentA);
+            };
+
+            it["dispatches OnComponentRemoved when removing a component"] = () => {
+                Entity eventEntity = null;
+                IComponent eventComponent = null;
+                _e.AddComponent(_componentA);
+                _e.OnComponentRemoved += (entity, component) => {
+                    eventEntity = entity;
+                    eventComponent = component;
+                };
+                _e.RemoveComponent(typeof(ComponentA));
+                eventEntity.should_be_same(_e);
+                eventComponent.should_be_same(_componentA);
+            };
+
+            it["dispatches OnComponentReplaced when replacing a component"] = () => {
+                Entity eventEntity = null;
+                IComponent eventComponent = null;
+                _e.AddComponent(_componentA);
+                _e.OnComponentReplaced += (entity, component) => {
+                    eventEntity = entity;
+                    eventComponent = component;
+                };
+                var newComponentA = new ComponentA();
+                _e.ReplaceComponent(newComponentA);
+                eventEntity.should_be_same(_e);
+                eventComponent.should_be_same(newComponentA);
+            };
+
+            it["doesn't dispatch OnComponentAdded when replacing a component"] = () => {
+                _e.OnComponentAdded += (entity, component) => fail();
+                _e.ReplaceComponent(_componentA);
+            };
+
+            it["doesn't dispatch OnComponentRemoved when replacing a component"] = () => {
+                _e.AddComponent(_componentA);
+                _e.OnComponentRemoved += (entity, component) => fail();
+                _e.ReplaceComponent(_componentA);
+            };
+
+            it["dispatches OnComponentRemoved when removing all components"] = () => {
+                var dispatched = 0;
+                _e.AddComponent(_componentA);
+                _e.AddComponent(_componentB);
+                _e.OnComponentRemoved += (entity, component) => dispatched++;
+                _e.RemoveAllComponents();
+                dispatched.should_be(2);
+            };
+        };
+
+        context["invalid operations"] = () => {
+            it["throws when adding a component of the same type twice"] = expect<EntityAlreadyHasComponentException>(() => {
+                _e.AddComponent(new ComponentA());
+                _e.AddComponent(new ComponentA());
+            });
+
+            it["throws when attempting to remove a component of type which hasn't been added"] = expect<EntityDoesNotHaveComponentException>(() => {
+                _e.RemoveComponent(typeof(ComponentA));
+            });
+
+            it["throws when attempting to get component of type which hasn't been added"] = expect<EntityDoesNotHaveComponentException>(() => {
+                _e.GetComponent(typeof(ComponentA));
+            });
+        };
+    }
+}
+
