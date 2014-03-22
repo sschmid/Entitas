e7299cca369dc00523d8e9d272b7e5fb54219cc8
Simon Schmid <mail@sschmid.com>
2014-03-22 21:44:31 +0100

    Added EntityArrayExtension

---
+++ Entitas/Entitas/Extensions/EntityArrayExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Entitas {
+    public static class EntityArrayExtension {
+        public static Entity[] With(this Entity[] entities, IEnumerable<Type> types) {
+            var with = new List<Entity>();
+            foreach (var e in entities)
+                if (e.HasComponents(types))
+                    with.Add(e);
+
+            return with.ToArray();
+        }
+
+        public static Entity[] Without(this Entity[] entities, IEnumerable<Type> types) {
+            var without = new List<Entity>();
+            foreach (var e in entities)
+                if (!e.HasAnyComponent(types))
+                    without.Add(e);
+
+            return without.ToArray();
+        }
+    }
+}
+
---
+++ Tests/Tests/describe_EntityArrayExtension.cs
@@ -0,0 +1,47 @@
+﻿using NSpec;
+using Entitas;
+
+class describe_EntityArrayExtension : nspec {
+    void when_filtering() {
+        Entity eA = null;
+        Entity eAB = null;
+        Entity eABC = null;
+        Entity[] allEntities = null;
+        before = () => {
+            eA = new Entity();
+            eA.AddComponent(new ComponentA());
+
+            eAB = new Entity();
+            eAB.AddComponent(new ComponentA());
+            eAB.AddComponent(new ComponentB());
+
+            eABC = new Entity();
+            eABC.AddComponent(new ComponentA());
+            eABC.AddComponent(new ComponentB());
+            eABC.AddComponent(new ComponentC());
+
+            allEntities = new[] { eA, eAB, eABC };
+        };
+
+        it["removes entities which have not specific components"] = () => {
+            var entities = allEntities.With(new [] { typeof(ComponentC) });
+            entities.Length.should_be(1);
+            entities.should_contain(eABC);
+        };
+
+        it["removes entities which have specific components"] = () => {
+            var entities = allEntities.Without(new [] { typeof(ComponentC) });
+            entities.Length.should_be(2);
+            entities.should_not_contain(eABC);
+        };
+
+        it["works great together"] = () => {
+            var entities = allEntities
+                .Without(new [] { typeof(ComponentC) })
+                .With(new [] { typeof(ComponentB) });
+            entities.Length.should_be(1);
+            entities.should_contain(eAB);
+        };
+    }
+}
+
