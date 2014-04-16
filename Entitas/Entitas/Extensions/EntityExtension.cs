4498a3d6e6876eba4cb98ad33ddca213236ada00
Simon Schmid <mail@sschmid.com>
2014-04-16 18:09:00 +0200

    Added EntityExtension and EntityRepositoryExtension

---
+++ Entitas/Entitas/Extensions/EntityExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace Entitas {
+    public static class EntityExtension {
+        public static void HasComponents(this Entity entity, params int[] indices) {
+            entity.HasComponents(indices);
+        }
+
+        public static void HasAnyComponent(this Entity entity, params int[] indices) {
+            entity.HasAnyComponent(indices);
+        }
+    }
+}
+
---
+++ Entitas/Entitas/Extensions/EntityRepositoryExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace Entitas {
+    public static class EntityRepositoryExtension {
+        public static EntityCollection GetCollection(this EntityRepository repo, params int[] indices) {
+            return repo.GetCollection(EntityMatcher.AllOf(indices));
+        }
+    }
+}
+
