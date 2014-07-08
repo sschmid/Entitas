b0cde20f2bfb479f888b8d43659088c62bec0d39
Simon Schmid <mail@sschmid.com>
2014-07-08 10:03:08 +0200

    Fixed travis.yml

--- runTests.sh
+++ runTests.sh
@@ -2,7 +2,7 @@
 xbuild Entitas.sln /verbosity:minimal
 if [ $? = 0 ]
 then
-	mono Tests/Libraries/nspec/NSpecRunner.exe Tests/bin/Debug/Tests.dll
+	mono Tests/Libraries/NSpec/NSpecRunner.exe Tests/bin/Debug/Tests.dll
 else
 	echo "WARNING: Could not compile!"
 fi
