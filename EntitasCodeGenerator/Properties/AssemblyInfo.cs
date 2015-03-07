794628e0c2a12951c06f1a4e33b5d93e1e2d5515
Simon Schmid <mail@sschmid.com>
2015-03-07 23:39:41 +0100

    Renamed CodeGenerator to EntitasCodeGenerator

--- EntitasCodeGenerator/Properties/AssemblyInfo.cs
+++ EntitasCodeGenerator/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("CodeGenerator")]
+[assembly: AssemblyTitle("EntitasCodeGenerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
