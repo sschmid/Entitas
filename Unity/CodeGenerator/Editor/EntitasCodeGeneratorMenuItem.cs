e5bb82d54ff02d65d3ffdc0fcfee955f845bc4b7
Simon Schmid <mail@sschmid.com>
2015-03-07 23:28:59 +0100

    Added EntitasCodeGeneratorMenuItem

--- Unity/CodeGenerator/Editor/EntitasCodeGeneratorMenuItem.cs
+++ Unity/CodeGenerator/Editor/EntitasCodeGeneratorMenuItem.cs
@@ -3,9 +3,9 @@ using Entitas;
 using Entitas.CodeGenerator;
 using UnityEditor;
 
-public static class MenuEntries {
+public static class EntitasCodeGeneratorMenuItem {
 
-    [MenuItem("Game/Entitas/Generate")]
+    [MenuItem("Entitas/Generate")]
     public static void EntitasGenerate() {
         var assembly = Assembly.GetAssembly(typeof(Entity));
         EntitasCodeGenerator.Generate(assembly, "Assets/Sources/Generated/");
