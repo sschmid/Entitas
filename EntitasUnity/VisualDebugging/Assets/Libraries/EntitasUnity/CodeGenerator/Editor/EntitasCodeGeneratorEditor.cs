eb77843eaf637ea7c6f28681c73cf24ab5e543d5
Simon Schmid <mail@sschmid.com>
2015-04-15 18:54:30 +0200

    Added Code Generator folder path in preferences

---
+++ EntitasUnity/VisualDebugging/Assets/Libraries/EntitasUnity/CodeGenerator/Editor/EntitasCodeGeneratorEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Entitas;
+using Entitas.CodeGenerator;
+using UnityEditor;
+using UnityEngine;
+
+public static class EntitasCodeGeneratorEditor {
+
+    const string defaultGeneratedFolder = "Assets/Sources/Generated/";
+    const string generatedFolderKey = "Entitas.CodeGenerator.GeneratedFolder";
+
+    [MenuItem("Entitas/Generate")]
+    public static void EntitasGenerate() {
+        var assembly = Assembly.GetAssembly(typeof(Entity));
+        var dir = EditorPrefs.GetString(generatedFolderKey, defaultGeneratedFolder);
+        EntitasCodeGenerator.Generate(assembly, dir);
+        AssetDatabase.Refresh();
+    }
+
+    [PreferenceItem("Entitas")]
+    public static void PreferenceItem() {
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        EditorGUILayout.LabelField("CodeGenerator", EditorStyles.boldLabel);
+        var dir = EditorPrefs.GetString(generatedFolderKey, defaultGeneratedFolder);
+        dir = EditorGUILayout.TextField("Generated Folder", dir);
+        EditorGUILayout.EndVertical();
+
+        if (GUI.changed) {
+            EditorPrefs.SetString(generatedFolderKey, dir);
+        }
+    }
+}
