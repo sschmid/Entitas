d47b4fecb4b25a72cac784a1627adc4fed006e98
Simon Schmid <mail@sschmid.com>
2015-04-24 10:11:23 +0200

    VisualDebugging: Added basic support to insert and remove elements to lists

--- Entitas.Unity.VisualDebugging/Assets/Entitas.Unity.VisualDebugging/Editor/EntityDebugEditor.cs
+++ Entitas.Unity.VisualDebugging/Assets/Entitas.Unity.VisualDebugging/Editor/EntityDebugEditor.cs
@@ -215,13 +215,51 @@ namespace Entitas.Unity.VisualDebugging {
             if (list == null) {
                 EditorGUILayout.LabelField(fieldName, "null");
             } else {
-                EditorGUILayout.LabelField(fieldName);
+                var elementType = list.GetType().GetGenericArguments()[0];
+                if (list.Count == 0) {
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.LabelField(fieldName);
+                    if (GUILayout.Button("Add element", GUILayout.Height(14))) {
+                        object defaultValue;
+                        if (createDefault(elementType, out defaultValue)) {
+                            list.Add(defaultValue);
+                        }
+                    }
+                    EditorGUILayout.EndHorizontal();
+                } else {
+                    EditorGUILayout.LabelField(fieldName);
+                }
+
                 var indent = EditorGUI.indentLevel;
                 EditorGUI.indentLevel = indent + 1;
-                var elementType = list.GetType().GetGenericArguments()[0];
+                Action editAction = null;
                 for (int i = 0; i < list.Count; i++) {
+                    EditorGUILayout.BeginHorizontal();
                     drawArrayItem(entity, index, component, list[i], elementType,
                         fieldName + "[" + i + "]", newValue => list[i] = newValue);
+                    if (GUILayout.Button("-", GUILayout.Width(19), GUILayout.Height(14))) {
+                        var removeAt = i;
+                        editAction = () => list.RemoveAt(removeAt);
+                    }
+                    if (GUILayout.Button("▴", GUILayout.Width(19), GUILayout.Height(14))) {
+                        object defaultValue;
+                        if (createDefault(elementType, out defaultValue)) {
+                            var insertAt = i;
+                            editAction = () => list.Insert(insertAt, defaultValue);
+                        }
+                    }
+                    if (GUILayout.Button("▾", GUILayout.Width(19), GUILayout.Height(14))) {
+                        object defaultValue;
+                        if (createDefault(elementType, out defaultValue)) {
+                            var insertAt = i + 1;
+                            editAction = () => list.Insert(insertAt, defaultValue);
+                        }
+                    }
+                    EditorGUILayout.EndHorizontal();
+                }
+
+                if (editAction != null) {
+                    editAction();
                 }
                 EditorGUI.indentLevel = indent;
             }
@@ -243,6 +281,21 @@ namespace Entitas.Unity.VisualDebugging {
                    (value != null && newValue == null) ||
                    ((value != null && newValue != null &&
                    !newValue.Equals(value)));
+        }
+
+        bool createDefault(Type type, out object defaultValue) {
+            if (type.IsValueType) {
+                defaultValue = Activator.CreateInstance(type);
+                return true;
+            }
+
+            if (type == typeof(string)) {
+                defaultValue = string.Empty;
+                return true;
+            }
+
+            defaultValue = null;
+            return false;
         }
     }
 }
