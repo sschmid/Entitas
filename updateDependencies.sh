45d6171fb16fb94d8ba4d54d3ce68682405dfdac
Simon Schmid <mail@sschmid.com>
2015-04-26 21:44:11 +0200

    updateDependencies.sh also copies dependencies to tests

--- updateDependencies.sh
+++ updateDependencies.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-# ./runTests.sh
+./buildPackage.sh
 if [ $? = 0 ]
 then
-	WD=$(pwd)
 	BIN_DIR="bin"
 	LIBRARIES_DIR_NAME="Libraries"
 
 	UNITY_CODE_GENERATOR_LIBRARIES_DIR="Entitas.Unity.CodeGenerator/Assets/${LIBRARIES_DIR_NAME}"
 	UNITY_VISUAL_DEBUGGING_LIBRARIES_DIR="Entitas.Unity.VisualDebugging/Assets/${LIBRARIES_DIR_NAME}"
+	TESTS_LIBRARIES_DIR="Tests/Libraries"
 
 	echo "CLEAN **************************************************************************"
 	rm -rfv $UNITY_CODE_GENERATOR_LIBRARIES_DIR
@@ -25,7 +25,11 @@ then
 	cp -rv "${BIN_DIR}/Entitas.Unity" $UNITY_VISUAL_DEBUGGING_LIBRARIES_DIR
 	cp -rv "${BIN_DIR}/Entitas.Unity.CodeGenerator" $UNITY_VISUAL_DEBUGGING_LIBRARIES_DIR
 
-	echo "DONE ***************************************************************************"
+	cp -rv "${BIN_DIR}/Entitas.Unity" $TESTS_LIBRARIES_DIR
+	cp -rv "${BIN_DIR}/Entitas.Unity.CodeGenerator" $TESTS_LIBRARIES_DIR
+	cp -rv "${BIN_DIR}/Entitas.Unity.VisualDebugging" $TESTS_LIBRARIES_DIR
+
+echo "DONE ***************************************************************************"
 else
 	echo "ERROR: Tests didn't pass!"
 	exit 1
