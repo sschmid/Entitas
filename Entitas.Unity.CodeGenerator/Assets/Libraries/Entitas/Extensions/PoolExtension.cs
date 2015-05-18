d2efb3ac3ad65bb65bcf437a1a8e3ac66f9d9da3
Simon Schmid <mail@sschmid.com>
2015-05-18 22:10:01 +0200

    Re-combined pool extensions for creating systems to pool.CreateSystem() and removed pool.CreateStartSystem() and pool. CreateExecuteSystem()

--- Entitas.Unity.CodeGenerator/Assets/Libraries/Entitas/Extensions/PoolExtension.cs
+++ Entitas.Unity.CodeGenerator/Assets/Libraries/Entitas/Extensions/PoolExtension.cs
@@ -11,42 +11,17 @@ namespace Entitas {
             return pool.GetGroup(matcher).GetEntities();
         }
 
-        public static IStartSystem CreateStartSystem<T>(this Pool pool) where T: ISystem, new() {
-            return pool.CreateStartSystem(typeof(T));
+        public static ISystem CreateSystem<T>(this Pool pool) where T: ISystem, new() {
+            return pool.CreateSystem(typeof(T));
         }
 
-        public static IStartSystem CreateStartSystem(this Pool pool, Type systemType) {
+        public static ISystem CreateSystem(this Pool pool, Type systemType) {
             var system = (ISystem)Activator.CreateInstance(systemType);
-
-            var startSystem = system as IStartSystem;
-            if (startSystem != null) {
-                setPool(system, pool);
-                return (IStartSystem)system;
-            }
-
-            throw new Exception("Cannot create IStartSystem for " + systemType);
-        }
-
-        public static IExecuteSystem CreateExecuteSystem<T>(this Pool pool) where T: ISystem, new() {
-            return pool.CreateExecuteSystem(typeof(T));
-        }
-
-        public static IExecuteSystem CreateExecuteSystem(this Pool pool, Type systemType) {
-            var system = (ISystem)Activator.CreateInstance(systemType);
-
-            var executeSystem = system as IExecuteSystem;
-            if (executeSystem != null) {
-                setPool(system, pool);
-                return (IExecuteSystem)system;
-            }
-
+            setPool(system, pool);
             var reactiveSystem = system as IReactiveSystem;
-            if (reactiveSystem != null) {
-                setPool(system, pool);
-                return new ReactiveSystem(pool, reactiveSystem);
-            }
-
-            throw new Exception("Cannot create IExecuteSystem for " + systemType);
+            return reactiveSystem != null
+                    ? new ReactiveSystem(pool, reactiveSystem)
+                    : system;
         }
 
         static void setPool(ISystem system, Pool pool) {
