722e2bcf2bbe5a4c7059e4f5c6cff15d6dd9627a
Simon Schmid <mail@sschmid.com>
2015-08-28 18:58:50 +0200

    buildPackage.sh includes HierarchyIcon.png.meta

--- Entitas.Unity.VisualDebugging/Assets/Entitas.Unity.VisualDebugging/Editor/HierarchyIconEditor.cs
+++ Entitas.Unity.VisualDebugging/Assets/Entitas.Unity.VisualDebugging/Editor/HierarchyIconEditor.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 namespace Entitas.Unity.VisualDebugging {
@@ -9,7 +8,7 @@ namespace Entitas.Unity.VisualDebugging {
         static Texture2D hierarchyIcon {
             get {
                 if (_hierarchyIcon == null) {
-                    var guid = AssetDatabase.FindAssets("l:Entitas-HierarchyIcon").SingleOrDefault();
+                    var guid = AssetDatabase.FindAssets("l:Entitas-HierarchyIcon")[0];
                     if (guid != null) {
                         var path = AssetDatabase.GUIDToAssetPath(guid);
                         _hierarchyIcon = AssetDatabase.LoadAssetAtPath<Texture2D>(path);
--- buildPackage.sh
+++ buildPackage.sh
@@ -23,14 +23,17 @@ then
 
 	echo "  Copy sources to temp"
 	cp -r {$ES"/"$ES,$CG"/"$CG,$ESU"/Assets/"$ESU,$UCG"/Assets/"$UCG,$UVD"/Assets/"$UVD} $TMP_DIR
-	cp $MIG"/bin/Release/Entitas.Migration.exe" ${TMP_DIR}"/MigrationAssistant.exe"
-	cp README.md ${TMP_DIR}/README.md
-	cp RELEASE_NOTES.md ${TMP_DIR}/RELEASE_NOTES.md
-	cp EntitasUpgradeGuide.md ${TMP_DIR}/EntitasUpgradeGuide.md
-	
+	cp $MIG"/bin/Release/Entitas.Migration.exe" $TMP_DIR"/MigrationAssistant.exe"
+	cp README.md $TMP_DIR/README.md
+	cp RELEASE_NOTES.md $TMP_DIR/RELEASE_NOTES.md
+	cp EntitasUpgradeGuide.md $TMP_DIR/EntitasUpgradeGuide.md
+
 	echo "  Remove ignored files"
 	find "./"$TMP_DIR -name "*.meta" -type f -delete
 	find "./"$TMP_DIR -name "*.DS_Store" -type f -delete
+
+	ICON_META=$UVD"/Editor/HierarchyIcon.png.meta"
+	cp $UVD"/Assets/"$ICON_META $TMP_DIR/$ICON_META
 
 	echo "  Create zip archive"
 	cd $TMP_DIR
