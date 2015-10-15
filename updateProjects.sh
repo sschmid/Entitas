22d83000a3f7e6713045aeb5521a14f2b2d4aa81
Simon Schmid <mail@sschmid.com>
2015-10-15 21:58:26 +0200

    Updated updateProjects.sh

--- updateProjects.sh
+++ updateProjects.sh
@@ -4,7 +4,7 @@ if [ $? = 0 ]
 then
 	echo "Update projects"
 
-	BIN_DIR="bin"
+	BIN_DIR=bin
 
 	ES="Entitas"
 	CG=$ES".CodeGenerator"
@@ -12,22 +12,22 @@ then
 	UCG=$ESU".CodeGenerator"
 	UVD=$ESU".VisualDebugging"
 
-	ESU_LIBS_DIR=$ESU"/Assets/Libraries"
-	UCODEGEN_LIBS_DIR=$UCG"/Assets/Libraries"
-	UVD_LIBS_DIR=$UVD"/Assets/Libraries"
-	TESTS_LIBS_DIR="Tests/Libraries"
+	ESU_LIBS_DIR=$ESU/Assets/Libraries
+	UCODEGEN_LIBS_DIR=$UCG/Assets/Libraries
+	UVD_LIBS_DIR=$UVD/Assets/Libraries
+	TESTS_LIBS_DIR=Tests/Libraries
 
 	echo "  Clean target folders"
-	find "./"$ESU_LIBS_DIR -type f -name "*.cs" -delete
-	find "./"$UCODEGEN_LIBS_DIR -type f -name "*.cs" -delete
-	find "./"$UVD_LIBS_DIR -type f -name "*.cs" -delete
-	rm -rf $TESTS_LIBS_DIR"/"{$ESU,$UCG,$UVD}
+	find ./$ESU_LIBS_DIR -type f -name "*.cs" -delete
+	find ./$UCODEGEN_LIBS_DIR -type f -name "*.cs" -delete
+	find ./$UVD_LIBS_DIR -type f -name "*.cs" -delete
+	rm -rf $TESTS_LIBS_DIR/{$ESU,$UCG,$UVD}
 
 	echo "  Copy sources"
-	cp -r $BIN_DIR"/"$ES $ESU_LIBS_DIR
-	cp -r $BIN_DIR"/"{$ES,$CG,$ESU} $UCODEGEN_LIBS_DIR
-	cp -r $BIN_DIR"/"{$ES,$CG,$ESU,$UCG} $UVD_LIBS_DIR
-	cp -r $BIN_DIR"/"{$ESU,$UCG,$UVD} $TESTS_LIBS_DIR
+	cp -r $BIN_DIR/$ES $ESU_LIBS_DIR
+	cp -r $BIN_DIR/{$ES,$CG,$ESU} $UCODEGEN_LIBS_DIR
+	cp -r $BIN_DIR/{$ES,$CG,$ESU,$UCG} $UVD_LIBS_DIR
+	cp -r $BIN_DIR/{$ESU,$UCG,$UVD} $TESTS_LIBS_DIR
 
 	echo "Done."
 else
