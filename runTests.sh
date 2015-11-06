76219e673381561fca95bad187e35196ebada951
Simon Schmid <mail@sschmid.com>
2015-11-07 00:04:24 +0100

    Merged shell scripts

--- runTests.sh
+++ runTests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xbuild Tests/Tests.csproj /verbosity:minimal
+xbuild Tests/Tests.csproj /property:Configuration=Debug /verbosity:minimal
 if [ $? = 0 ]
 then
 	mono Tests/Libraries/NSpec/NSpecRunner.exe Tests/bin/Debug/Tests.dll
