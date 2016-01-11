ef671f4c0e920da3d763db08aea1c10201e39b36
Simon Schmid <mail@sschmid.com>
2016-01-11 22:51:26 +0100

    Added EntitasFeedback

---
+++ Entitas.Unity.VisualDebugging/Assets/Entitas.Unity.VisualDebugging/Editor/EntitasFeedback.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Entitas.Unity.VisualDebugging {
+    public static class EntitasFeedback {
+
+        [MenuItem("Entitas/Feedback/Report a bug...", false, 30)]
+        public static void ReportBug() {
+            Application.OpenURL("https://github.com/sschmid/Entitas-CSharp/issues");
+        }
+
+        [MenuItem("Entitas/Feedback/Request a feature...", false, 31)]
+        public static void RequestFeature() {
+            Application.OpenURL("https://github.com/sschmid/Entitas-CSharp/issues");
+        }
+
+        [MenuItem("Entitas/Feedback/Join the Entitas chat...", false, 32)]
+        public static void EntitasChat() {
+            Application.OpenURL("https://gitter.im/sschmid/Entitas-CSharp");
+        }
+
+        [MenuItem("Entitas/Feedback/Entitas wiki...", false, 32)]
+        public static void EntitasWiki() {
+            Application.OpenURL("https://github.com/sschmid/Entitas-CSharp/wiki");
+        }
+    }
+}
+
