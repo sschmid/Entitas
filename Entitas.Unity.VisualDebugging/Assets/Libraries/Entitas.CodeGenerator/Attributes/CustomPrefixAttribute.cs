5fddbb98f0389bd94aaa12a1cfdeb4644ca28c78
Simon Schmid <mail@sschmid.com>
2016-01-13 21:45:11 +0100

    Updated projects

---
+++ Entitas.Unity.VisualDebugging/Assets/Libraries/Entitas.CodeGenerator/Attributes/CustomPrefixAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Entitas.CodeGenerator {
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CustomPrefixAttribute : Attribute {
+        public string prefix;
+
+        public CustomPrefixAttribute(string prefix) {
+            this.prefix = prefix;
+        }
+    }
+}
