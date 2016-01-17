6e65c3b4fb12484fbb6efffb06d555d79a5863e0
Simon Schmid <mail@sschmid.com>
2016-01-17 19:42:26 +0100

    Updated projects

---
+++ Tests/Libraries/Entitas.Unity.VisualDebugging/DebugSystems/SystemInfo.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Entitas;
+using Entitas.Unity.VisualDebugging;
+
+namespace Entitas.Unity.VisualDebugging {
+    public class SystemInfo {
+        public ISystem system { get { return _system; } }
+        public string systemName { get { return _systemName; } }
+
+        public bool isInitializeSystems {
+            get { return (_interfaceFlags & SystemInterfaceFlags.IInitializeSystem) == SystemInterfaceFlags.IInitializeSystem; }
+        }
+
+        public bool isExecuteSystems {
+            get { return (_interfaceFlags & SystemInterfaceFlags.IExecuteSystem) == SystemInterfaceFlags.IExecuteSystem; }
+        }
+
+        public bool isReactiveSystems {
+            get { return (_interfaceFlags & SystemInterfaceFlags.IReactiveSystem) == SystemInterfaceFlags.IReactiveSystem; }
+        }
+
+        public double accumulatedExecutionDuration { get { return _accumulatedExecutionDuration; } }
+        public double minExecutionDuration { get { return _minExecutionDuration; } }
+        public double maxExecutionDuration { get { return _maxExecutionDuration; } }
+        public double averageExecutionDuration {
+            get { return _durationsCount == 0 ? 0 : _accumulatedExecutionDuration / _durationsCount; }
+        }
+
+        public bool isActive;
+
+        readonly ISystem _system;
+        readonly SystemInterfaceFlags _interfaceFlags;
+        readonly string _systemName;
+
+        double _accumulatedExecutionDuration;
+        double _minExecutionDuration;
+        double _maxExecutionDuration;
+        int _durationsCount;
+
+        const string SYSTEM_SUFFIX = "System";
+
+        public SystemInfo(ISystem system) {
+            _system = system;
+
+            var reactiveSystem = system as ReactiveSystem;
+            var isReactive = reactiveSystem != null;
+            Type systemType;
+            if (isReactive) {
+                _interfaceFlags = getInterfaceFlags(reactiveSystem.subsystem, isReactive);
+                systemType = reactiveSystem.subsystem.GetType();
+            } else {
+                _interfaceFlags = getInterfaceFlags(system, isReactive);
+                systemType = system.GetType();
+            }
+
+            var debugSystem = system as DebugSystems;
+            if (debugSystem != null) {
+                _systemName = debugSystem.name;
+            } else {
+                _systemName = systemType.Name.EndsWith(SYSTEM_SUFFIX, StringComparison.Ordinal)
+                    ? systemType.Name.Substring(0, systemType.Name.Length - SYSTEM_SUFFIX.Length)
+                    : systemType.Name;
+            }
+
+            isActive = true;
+        }
+
+        public void AddExecutionDuration(double executionDuration) {
+            if (executionDuration < _minExecutionDuration || _minExecutionDuration == 0) {
+                _minExecutionDuration = executionDuration;
+            }
+            if (executionDuration > _maxExecutionDuration) {
+                _maxExecutionDuration = executionDuration;
+            }
+
+            _accumulatedExecutionDuration += executionDuration;
+            _durationsCount += 1;
+        }
+
+        public void ResetDurations() {
+            _accumulatedExecutionDuration = 0;
+            _durationsCount = 0;
+        }
+
+        static SystemInterfaceFlags getInterfaceFlags(ISystem system, bool isReactive) {
+            var flags = SystemInterfaceFlags.None;
+            if (system is IInitializeSystem) {
+                flags |= SystemInterfaceFlags.IInitializeSystem;
+            }
+            if (system is IExecuteSystem) {
+                flags |= SystemInterfaceFlags.IExecuteSystem;
+            }
+            if (isReactive) {
+                flags |= SystemInterfaceFlags.IReactiveSystem;
+            }
+
+            return flags;
+        }
+
+        [Flags]
+        enum SystemInterfaceFlags {
+            None = 0,
+            IInitializeSystem = 1,
+            IExecuteSystem = 2,
+            IReactiveSystem = 4
+        }
+    }
+}
--- UnityTests/Assets/Libraries/Entitas.Unity.VisualDebugging/DebugSystems/DebugSystems.cs
+++ UnityTests/Assets/Libraries/Entitas.Unity.VisualDebugging/DebugSystems/DebugSystems.cs
@@ -1,78 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using UnityEngine;
 
 namespace Entitas.Unity.VisualDebugging {
 
-    public class SystemInfo {
-        public ISystem system { get { return _system; } }
-        public string systemName { get { return _systemName; } }
-        public double totalExecutionDuration { get { return _totalExecutionDuration; } }
-        public double minExecutionDuration { get { return _minExecutionDuration; } }
-        public double maxExecutionDuration { get { return _maxExecutionDuration; } }
-        public double averageExecutionDuration {
-            get { return _durationsCount == 0 ? 0 : _totalExecutionDuration / _durationsCount; }
-        }
-
-        public bool isActive;
-
-        ISystem _system;
-        string _systemName;
-        double _totalExecutionDuration = -1;
-        double _minExecutionDuration;
-        double _maxExecutionDuration;
-        int _durationsCount;
-
-        const string SYSTEM_SUFFIX = "System";
-
-        public SystemInfo(ISystem system, Type systemType) {
-            _system = system;
-
-            var debugSystem = system as DebugSystems;
-            if (debugSystem != null) {
-                _systemName = debugSystem.name;
-            } else {
-                _systemName = systemType.Name.EndsWith(SYSTEM_SUFFIX, StringComparison.Ordinal)
-                    ? systemType.Name.Substring(0, systemType.Name.Length - SYSTEM_SUFFIX.Length)
-                    : systemType.Name;
-            }
-            
-            isActive = true;
-        }
-
-        public void AddExecutionDuration(double executionDuration) {
-            if (executionDuration < _minExecutionDuration || _totalExecutionDuration == -1) {
-                _minExecutionDuration = executionDuration;
-                if (_totalExecutionDuration == -1) {
-                    _totalExecutionDuration = 0;
-                }
-            }
-            if (executionDuration > _maxExecutionDuration) {
-                _maxExecutionDuration = executionDuration;
-            }
-
-            _totalExecutionDuration += executionDuration;
-            _durationsCount += 1;
-        }
-
-        public void Reset() {
-            _totalExecutionDuration = 0;
-            _durationsCount = 0;
-        }
-    }
-
     public enum AvgResetInterval {
-        EveryFrame = 1,
-        Every30Frames = 30,
-        Every60Frames = 60,
-        Every120Frames = 120,
-        Every300Frames = 300,
+        Always = 1,
+        VeryFast = 30,
+        Fast = 60,
+        Normal = 120,
+        Slow = 300,
         Never = int.MaxValue
     }
 
     public class DebugSystems : Systems {
+        public static AvgResetInterval avgResetInterval = AvgResetInterval.Never;
+
         public int initializeSystemsCount { get { return _initializeSystems.Count; } }
         public int executeSystemsCount { get { return _executeSystems.Count; } }
         public int totalSystemsCount { get { return _systems.Count; } }
@@ -80,30 +24,28 @@ namespace Entitas.Unity.VisualDebugging {
         public string name { get { return _name; } }
         public GameObject container { get { return _container.gameObject; } }
         public double totalDuration { get { return _totalDuration; } }
-        public SystemInfo[] systemInfos {
-            get { return _systemInfos.Values
-                    .Where(systemInfo => systemInfo.averageExecutionDuration >= threshold)
-                    .ToArray(); }
-        }
+        public SystemInfo[] initializeSystemInfos { get { return _initializeSystemInfos.ToArray(); } }
+        public SystemInfo[] executeSystemInfos { get { return _executeSystemInfos.ToArray(); } }
 
         public bool paused;
 
-        public float threshold;
-        public AvgResetInterval avgResetInterval = AvgResetInterval.Never;
+
+        readonly string _name;
 
         readonly List<ISystem> _systems;
-        readonly string _name;
         readonly Transform _container;
+        readonly List<SystemInfo> _initializeSystemInfos;
+        readonly List<SystemInfo> _executeSystemInfos;
+        readonly Stopwatch _stopwatch;
         double _totalDuration;
-        Dictionary<Type, SystemInfo> _systemInfos;
-        Stopwatch _stopwatch;
 
         public DebugSystems(string name = "Systems") {
             _name = name;
             _systems = new List<ISystem>();
             _container = new GameObject().transform;
             _container.gameObject.AddComponent<DebugSystemsBehaviour>().Init(this);
-            _systemInfos = new Dictionary<Type, SystemInfo>();
+            _initializeSystemInfos = new List<SystemInfo>();
+            _executeSystemInfos = new List<SystemInfo>();
             _stopwatch = new Stopwatch();
             updateName();
         }
@@ -115,12 +57,27 @@ namespace Entitas.Unity.VisualDebugging {
                 debugSystems.container.transform.SetParent(_container.transform, false);
             }
 
+            var systemInfo = new SystemInfo(system);
+            if (systemInfo.isInitializeSystems) {
+                _initializeSystemInfos.Add(new SystemInfo(system));
+            }
+            if (systemInfo.isExecuteSystems || systemInfo.isReactiveSystems) {
+                _executeSystemInfos.Add(new SystemInfo(system));
+            }
+
             return base.Add(system);
         }
 
-        public void Reset() {
-            foreach (var systemInfo in _systemInfos.Values) {
-                systemInfo.Reset();
+        public void ResetDurations() {
+            foreach (var systemInfo in _initializeSystemInfos) {
+                systemInfo.ResetDurations();
+            }
+            foreach (var systemInfo in _executeSystemInfos) {
+                systemInfo.ResetDurations();
+                var debugSystems = systemInfo.system as DebugSystems;
+                if (debugSystems != null) {
+                    debugSystems.ResetDurations();
+                }
             }
         }
 
@@ -128,7 +85,7 @@ namespace Entitas.Unity.VisualDebugging {
             _totalDuration = 0;
             for (int i = 0, _initializeSystemsCount = _initializeSystems.Count; i < _initializeSystemsCount; i++) {
                 var system = _initializeSystems[i];
-                var systemInfo = getSystemInfo(system);
+                var systemInfo = _initializeSystemInfos[i];
                 if (systemInfo.isActive) {
                     var duration = monitorSystemInitializeDuration(system);
                     _totalDuration += duration;
@@ -148,11 +105,11 @@ namespace Entitas.Unity.VisualDebugging {
         public void Step() {
             _totalDuration = 0;
             if (Time.frameCount % (int)avgResetInterval == 0) {
-                Reset();
+                ResetDurations();
             }
             for (int i = 0, exeSystemsCount = _executeSystems.Count; i < exeSystemsCount; i++) {
                 var system = _executeSystems[i];
-                var systemInfo = getSystemInfo(system);
+                var systemInfo = _executeSystemInfos[i];
                 if (systemInfo.isActive) {
                     var duration = monitorSystemExecutionDuration(system);
                     _totalDuration += duration;
@@ -177,21 +134,6 @@ namespace Entitas.Unity.VisualDebugging {
             system.Execute();
             _stopwatch.Stop();
             return _stopwatch.Elapsed.TotalMilliseconds;
-        }
-
-        SystemInfo getSystemInfo(ISystem system) {
-            var reactiveSystem = system as ReactiveSystem;
-            var systemType = reactiveSystem != null
-                                ? reactiveSystem.subsystem.GetType()
-                                : system.GetType();
-
-            SystemInfo systemInfo;
-            if (!_systemInfos.TryGetValue(systemType, out systemInfo)) {
-                systemInfo = new SystemInfo(system, systemType);
-                _systemInfos.Add(systemType, systemInfo);
-            }
-
-            return systemInfo;
         }
 
         void updateName() {
