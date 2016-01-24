5c70f21ed88897a4d21342ac7f2a6c0c0db1dd66
Simon Schmid <mail@sschmid.com>
2016-01-24 14:35:10 +0100

    Added documentation to Group.cs and GroupExtension.cs

--- Entitas/Entitas/Extensions/GroupExtension.cs
+++ Entitas/Entitas/Extensions/GroupExtension.cs
@@ -1,5 +1,7 @@
 ﻿namespace Entitas {
     public static class GroupExtension {
+
+        /// Creates a GroupObserver for this group.
         public static GroupObserver CreateObserver(this Group group, GroupEventType eventType = GroupEventType.OnEntityAdded) {
             return new GroupObserver(group, eventType);
         }
