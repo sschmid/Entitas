250beca442c2dbae5fb117c1bd8b8ab1e9e3ef06
Simon Schmid <mail@sschmid.com>
2016-01-27 21:06:27 +0100

    Added documentation for interfaces

--- Entitas/Entitas/Interfaces/IInitializeSystem.cs
+++ Entitas/Entitas/Interfaces/IInitializeSystem.cs
@@ -1,4 +1,6 @@
 ﻿namespace Entitas {
+
+    /// Implement this interface if you want to create a system which should be initialized once in the beginning.
     public interface IInitializeSystem : ISystem {
         void Initialize();
     }
