3bc31db516b3742f33677d70368a22fb403eda72
Simon Schmid <mail@sschmid.com>
2016-01-27 21:05:56 +0100

    Added documentation for extensions

--- Entitas/Entitas/Extensions/IMatcherExtension.cs
+++ Entitas/Entitas/Extensions/IMatcherExtension.cs
@@ -1,13 +1,17 @@
 ﻿namespace Entitas {
     public static class IMatcherExtension {
+
+        /// Convenience method to create a new TriggerOnEvent. Commonly used in IReactiveSystem and IMultiReactiveSystem.
         public static TriggerOnEvent OnEntityAdded(this IMatcher matcher) {
             return new TriggerOnEvent(matcher, GroupEventType.OnEntityAdded);
         }
 
+        /// Convenience method to create a new TriggerOnEvent. Commonly used in IReactiveSystem and IMultiReactiveSystem.
         public static TriggerOnEvent OnEntityRemoved(this IMatcher matcher) {
             return new TriggerOnEvent(matcher, GroupEventType.OnEntityRemoved);
         }
 
+        /// Convenience method to create a new TriggerOnEvent. Commonly used in IReactiveSystem and IMultiReactiveSystem.
         public static TriggerOnEvent OnEntityAddedOrRemoved(this IMatcher matcher) {
             return new TriggerOnEvent(matcher, GroupEventType.OnEntityAddedOrRemoved);
         }
