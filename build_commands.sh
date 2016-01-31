ec86a6d8aada6e9ba5a3fd519fd4687f42b027b4
Simon Schmid <mail@sschmid.com>
2016-01-31 17:14:40 +0100

    Updated build_commands to not copy sources to tests project anymore

--- build_commands.sh
+++ build_commands.sh
@@ -31,17 +31,14 @@ update_project_dependencies() {
   UVD_LIBS_DIR="$UVD/Assets/Libraries"
   UMIG_LIBS_DIR="$UMIG/Assets/Libraries"
   UNITY_TESTS_LIBS_DIR="UnityTests/Assets/Libraries"
-  TESTS_LIBS_DIR="Tests/Libraries"
 
   find $ESU_LIBS_DIR $UCODEGEN_LIBS_DIR $UVD_LIBS_DIR $UMIG_LIBS_DIR $UNITY_TESTS_LIBS_DIR -type f -name "*.cs" -delete
-  rm -rf $TESTS_LIBS_DIR/{$ESU,$UCG,$UVD}
 
   cp -r $SRC_DIR/$ES $ESU_LIBS_DIR
   cp -r $SRC_DIR/{$ES,$CG,$ESU} $UCODEGEN_LIBS_DIR
   cp -r $SRC_DIR/{$ES,$CG,$ESU,$UCG} $UVD_LIBS_DIR
   cp -r $SRC_DIR/{$ES,$CG,$MIG,$ESU,$UCG,$UVD} $UMIG_LIBS_DIR
   cp -r $SRC_DIR/{$ES,$CG,$ESU,$UCG,$UVD} $UNITY_TESTS_LIBS_DIR
-  cp -r $SRC_DIR/{$ES,$CG,$ESU,$UCG,$UVD} $TESTS_LIBS_DIR
 
   echo "Updating project dependencies done."
 }
