ddb4862af6a0250a16fa56392e02f2ee1458862f
Simon Schmid <mail@sschmid.com>
2016-02-02 23:08:18 +0100

    Added Migration Window

--- Entitas.Unity.Migration/Assets/Entitas.Unity.Migration/Editor/EntitasMigration.cs
+++ Entitas.Unity.Migration/Assets/Entitas.Unity.Migration/Editor/EntitasMigration.cs
@@ -1,20 +1,55 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 using Entitas.Migration;
 using UnityEditor;
 using UnityEngine;
 
 namespace Entitas.Unity.Migration {
-    public static class EntitasMigration {
+    public class EntitasMigrationWindow : EditorWindow {
 
-        [MenuItem("Entitas/Migrate", false, 300)]
-        public static void Migrate() {
-            var allMigrations = new IMigration[] {
-                new M0180(),
-                new M0190(),
-                new M0220(),
-                new M0260()
-            };
+        [MenuItem("Entitas/Migrate...", false, 300)]
+        public static void OpenMigrate() {
+            EditorWindow.GetWindow<EntitasMigrationWindow>("Entitas Migration").Show();
+        }
 
+        IMigration[] _migrations;
+        Vector2 _scrollViewPosition;
+
+        void OnEnable() {
+            _migrations = getMigrations();
+        }
+
+        static IMigration[] getMigrations() {
+            return Assembly.GetAssembly(typeof(IMigration)).GetTypes()
+                .Where(type => type.GetInterfaces().Contains(typeof(IMigration)))
+                .OrderByDescending(type => type.Name)
+                .Select(type => (IMigration)Activator.CreateInstance(type))
+                .ToArray();
+        }
+
+        void OnGUI() {
+            _scrollViewPosition = EditorGUILayout.BeginScrollView(_scrollViewPosition);
+            {
+                var descriptionStyle = new GUIStyle(GUI.skin.label);
+                descriptionStyle.wordWrap = true;
+                foreach (var migration in _migrations) {
+                    EditorGUILayout.Space();
+                    EditorGUILayout.BeginVertical(GUI.skin.box);
+                    {
+                        EditorGUILayout.LabelField(migration.version, EditorStyles.boldLabel);
+                        EditorGUILayout.LabelField(migration.description, descriptionStyle);
+                        if (GUILayout.Button("Apply migration " + migration.version)) {
+                            migrate(migration);
+                        }
+                    }
+                    EditorGUILayout.EndVertical();
+                }
+            }
+            EditorGUILayout.EndScrollView();
+        }
+
+        void migrate(IMigration migration) {
             var shouldMigrate = EditorUtility.DisplayDialog("Migrate",
                                     "You are about to migrate your source files. " +
                                     "Make sure that you have committed your current project or that you have a backup of your project before you proceed.",
@@ -23,27 +58,22 @@ namespace Entitas.Unity.Migration {
                                 );
 
             if (shouldMigrate) {
-
                 EditorUtility.DisplayDialog("Migrate",
-                    "There are " + allMigrations.Length + " migration steps. Please select the folder requested in the title bar of the 'Select Folder' panel.",
-                    "I will select the requested folders"
+                    "Please select the folder, " + migration.workingDirectory + ".",
+                    "I will select the requested folder"
                 );
 
                 var path = "Assets/";
-                for (int i = 0; i < allMigrations.Length; i++) {
-                    var migration = allMigrations[i];
-                    var stepInfo = (i + 1) + "/" + allMigrations.Length;
-                    path = EditorUtility.OpenFolderPanel(stepInfo + ": " + migration.version + ": " + migration.workingDirectory, path, string.Empty);
-                    if (!string.IsNullOrEmpty(path)) {
-                        var changedFiles = migration.Migrate(path);
-                        Debug.Log("Applying " + migration.version);
-                        foreach (var file in changedFiles) {
-                            MigrationUtils.WriteFiles(changedFiles);
-                            Debug.Log("Migrated " + file.fileName);
-                        }
-                    } else {
-                        throw new Exception("Could not complete migration! Selected path was invalid!");
+                path = EditorUtility.OpenFolderPanel(migration.version + ": " + migration.workingDirectory, path, string.Empty);
+                if (!string.IsNullOrEmpty(path)) {
+                    var changedFiles = migration.Migrate(path);
+                    Debug.Log("Applying " + migration.version);
+                    foreach (var file in changedFiles) {
+                        MigrationUtils.WriteFiles(changedFiles);
+                        Debug.Log("Migrated " + file.fileName);
                     }
+                } else {
+                    throw new Exception("Could not complete migration! Selected path was invalid!");
                 }
             }
         }
