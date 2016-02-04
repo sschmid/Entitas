f65e30238a9e78146061a8978ca7f92ca5876b45
Simon Schmid <mail@sschmid.com>
2016-02-04 23:00:35 +0100

    Added toggle Entitas Preferences to enable or disable Visual Debugging

--- Entitas.Unity.VisualDebugging/Assets/Entitas.Unity.VisualDebugging/Editor/VisualDebuggingPreferencesDrawer.cs
+++ Entitas.Unity.VisualDebugging/Assets/Entitas.Unity.VisualDebugging/Editor/VisualDebuggingPreferencesDrawer.cs
@@ -1,4 +1,5 @@
-﻿using Entitas.Unity;
+﻿using System.Linq;
+using Entitas.Unity;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,16 +8,40 @@ namespace Entitas.Unity.VisualDebugging {
 
         public int priority { get { return 20; } }
 
+        const string ENTITAS_DISABLE_VISUAL_DEBUGGING = "ENTITAS_DISABLE_VISUAL_DEBUGGING";
+
         VisualDebuggingConfig _visualDebuggingConfig;
+        ScriptingDefineSymbols _scriptingDefineSymbols;
+
+        bool _enableVisualDebugging;
 
         public void Initialize(EntitasPreferencesConfig config) {
             _visualDebuggingConfig = new VisualDebuggingConfig(config);
+            _scriptingDefineSymbols = new ScriptingDefineSymbols();
+            _enableVisualDebugging = !_scriptingDefineSymbols.buildTargetToDefSymbol.Values
+                .All<string>(defs => defs.Contains(ENTITAS_DISABLE_VISUAL_DEBUGGING));
         }
 
         public void Draw(EntitasPreferencesConfig config) {
             EditorGUILayout.BeginVertical(GUI.skin.box);
             {
                 EditorGUILayout.LabelField("VisualDebugging", EditorStyles.boldLabel);
+
+                EditorGUI.BeginChangeCheck();
+                {
+                    _enableVisualDebugging = EditorGUILayout.Toggle("Enable Visual Debugging", _enableVisualDebugging);
+                }
+                var changed = EditorGUI.EndChangeCheck();
+
+                if (changed) {
+                    if (_enableVisualDebugging) {
+                        _scriptingDefineSymbols.RemoveDefineSymbol(ENTITAS_DISABLE_VISUAL_DEBUGGING);
+                    } else {
+                        _scriptingDefineSymbols.AddDefineSymbol(ENTITAS_DISABLE_VISUAL_DEBUGGING);
+                    }
+                }
+
+                EditorGUILayout.Space();
 
                 _visualDebuggingConfig.defaultInstanceCreatorFolderPath =
                     EditorGUILayout.TextField("DefaultInstanceCreator Folder", _visualDebuggingConfig.defaultInstanceCreatorFolderPath);
