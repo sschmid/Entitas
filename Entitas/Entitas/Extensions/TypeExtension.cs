b24e37ff3bb98f97b6f00e78fb01bbdffdc32011
Simon Schmid <mail@sschmid.com>
2016-02-15 21:07:17 +0100

    Added TypeExtension

---
+++ Entitas/Entitas/Extensions/TypeExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Entitas {
+    public static class TypeExtension {
+        public static bool ImplementsInterface<T>(this Type type) {
+            if (!type.IsInterface && type.GetInterfaces().Contains(typeof(T))) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
