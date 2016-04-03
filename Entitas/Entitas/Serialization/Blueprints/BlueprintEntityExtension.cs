75eff2014944146a3ba67a8a484b74eba66675b0
Simon Schmid <mail@sschmid.com>
2016-04-03 22:37:31 +0200

    entity.ApplyBlueprint() can optionally replace existing components.

--- Entitas/Entitas/Serialization/Blueprints/BlueprintEntityExtension.cs
+++ Entitas/Entitas/Serialization/Blueprints/BlueprintEntityExtension.cs
@@ -4,10 +4,16 @@ namespace Entitas {
 
     public partial class Entity {
 
-        public Entity ApplyBlueprint(Blueprint blueprint) {
+        /// Adds all components from the blueprint to the entity.
+        /// When 'replaceComponents' is set to true entity.ReplaceComponent() will be used instead of entity.AddComponent().
+        public Entity ApplyBlueprint(Blueprint blueprint, bool replaceComponents = false) {
             for (int i = 0, componentsLength = blueprint.components.Length; i < componentsLength; i++) {
                 var componentBlueprint = blueprint.components[i];
-                AddComponent(componentBlueprint.index, componentBlueprint.CreateComponent(this));
+                if (replaceComponents) {
+                    ReplaceComponent(componentBlueprint.index, componentBlueprint.CreateComponent(this));
+                } else {
+                    AddComponent(componentBlueprint.index, componentBlueprint.CreateComponent(this));
+                }
             }
 
             return this;
