688a2651901cedfa56ca6c6f35c13d7821162bcc
Simon Schmid <mail@sschmid.com>
2016-04-15 16:00:57 +0200

    Added missing namespace

--- Entitas.Unity/Assets/Entitas/Unity/Serialization/Blueprints/Editor/BinaryBlueprintPostprocessor.cs
+++ Entitas.Unity/Assets/Entitas/Unity/Serialization/Blueprints/Editor/BinaryBlueprintPostprocessor.cs
@@ -2,17 +2,20 @@
 using Entitas.Unity.Serialization.Blueprints;
 using UnityEditor;
 
-public class BinaryBlueprintPostprocessor : AssetPostprocessor {
+namespace Entitas.Unity.Serialization.Blueprints {
+    
+    public class BinaryBlueprintPostprocessor : AssetPostprocessor {
 
-    public const string ASSET_LABEL = "EntitasBinaryBlueprint";
+        public const string ASSET_LABEL = "EntitasBinaryBlueprint";
 
-    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromPath) {
-        foreach (string assetPath in importedAssets) {
-            var asset = AssetDatabase.LoadAssetAtPath<BinaryBlueprint>(assetPath);
-            var labels = new List<string>(AssetDatabase.GetLabels(asset));
-            if (!labels.Contains(ASSET_LABEL)) {
-                labels.Add(ASSET_LABEL);
-                AssetDatabase.SetLabels(asset, labels.ToArray());
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromPath) {
+            foreach (string assetPath in importedAssets) {
+                var asset = AssetDatabase.LoadAssetAtPath<BinaryBlueprint>(assetPath);
+                var labels = new List<string>(AssetDatabase.GetLabels(asset));
+                if (!labels.Contains(ASSET_LABEL)) {
+                    labels.Add(ASSET_LABEL);
+                    AssetDatabase.SetLabels(asset, labels.ToArray());
+                }
             }
         }
     }
