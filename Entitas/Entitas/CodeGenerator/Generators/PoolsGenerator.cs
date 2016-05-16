05b64fb60ff57fe1403d014db37db6c703611ca9
Simon Schmid <mail@sschmid.com>
2016-05-16 22:59:11 +0200

    Reduced using of expectedCode .ToUnixLineEndings()

--- Entitas/Entitas/CodeGenerator/Generators/PoolsGenerator.cs
+++ Entitas/Entitas/CodeGenerator/Generators/PoolsGenerator.cs
@@ -61,7 +61,7 @@ public static class Pools {{{0}{1}
                 : poolNames.Aggregate(string.Empty, (acc, poolName) =>
                     acc + string.Format(GETTER, poolName.LowercaseFirst(), poolName, poolName + " ", getIndexKeys(poolName, infos)));
 
-            return new [] { new CodeGenFile(FILE_NAME, string.Format(CLASS_TEMPLATE, allPools, getters).ToUnixLineEndings(), GetType().FullName) };
+            return new [] { new CodeGenFile(FILE_NAME, string.Format(CLASS_TEMPLATE, allPools, getters), GetType().FullName) };
         }
 
         static string getIndexKeys(string poolName, ComponentInfo[] infos) {
--- Tests/Tests/Entitas.Unity/Blueprints/describe_BlueprintsGenerator.cs
+++ Tests/Tests/Entitas.Unity/Blueprints/describe_BlueprintsGenerator.cs
@@ -1,32 +1,31 @@
-﻿using Entitas;
-using Entitas.CodeGenerator;
+﻿using Entitas.CodeGenerator;
 using NSpec;
 
 class describe_BlueprintsGenerator : nspec {
 
+    static CodeGenFile[] generate(params string[] blueprints) {
+        return new BlueprintsGenerator().Generate(blueprints);
+    }
+
+    static void generateFile(string expectedCode, params string[] blueprints) {
+        expectedCode = expectedCode.ToUnixLineEndings();
+        var files = new BlueprintsGenerator().Generate(blueprints);
+        files[0].fileContent.should_be(expectedCode);
+    }
+
     void when_generating() {
 
-        BlueprintsGenerator generator = null;
-        before = () => {
-            generator = new BlueprintsGenerator();
-        };
-
         it["doesn't create a Blueprints file if there aren't any blueprints"] = () => {
-            var files = generator.Generate(new string[0]);
-            files.Length.should_be(0);
+            generate(new string[0]).Length.should_be(0);
         };
 
         it["adds getters for a blueprint to Blueprints ordered by name"] = () => {
-            const string name1 = "MyBlueprint1";
-            const string name2 = "MyBlueprint2";
-            var blueprintNames = new [] { name2, name1 };
-
-            var files = generator.Generate(blueprintNames);
+            var files = generate("MyBlueprint1", "MyBlueprint2");
             files.Length.should_be(1);
 
             var file = files[0];
             file.fileName.should_be("BlueprintsGeneratedExtension");
-            file.fileContent.ToUnixLineEndings().should_be(@"using Entitas.Serialization.Blueprints;
+            file.fileContent.should_be(@"using Entitas.Serialization.Blueprints;
 
 namespace Entitas.Unity.Serialization.Blueprints {
     public partial class Blueprints {
@@ -34,43 +33,40 @@ namespace Entitas.Unity.Serialization.Blueprints {
         public Blueprint MyBlueprint2 { get { return GetBlueprint(""MyBlueprint2""); } }
     }
 }
-");
+".ToUnixLineEndings());
         };
 
         it["removes whitespace for getter names"] = () => {
-            var files = generator.Generate(new[] { "My Blueprint" });
-            files[0].fileContent.ToUnixLineEndings().should_be(@"using Entitas.Serialization.Blueprints;
+            generateFile(@"using Entitas.Serialization.Blueprints;
 
 namespace Entitas.Unity.Serialization.Blueprints {
     public partial class Blueprints {
         public Blueprint MyBlueprint { get { return GetBlueprint(""My Blueprint""); } }
     }
 }
-");
+", "My Blueprint");
         };
 
         it["removes '-' for getter names"] = () => {
-            var files = generator.Generate(new[] { "My-Blueprint" });
-            files[0].fileContent.ToUnixLineEndings().should_be(@"using Entitas.Serialization.Blueprints;
+            generateFile(@"using Entitas.Serialization.Blueprints;
 
 namespace Entitas.Unity.Serialization.Blueprints {
     public partial class Blueprints {
         public Blueprint MyBlueprint { get { return GetBlueprint(""My-Blueprint""); } }
     }
 }
-");
+", "My-Blueprint");
         };
 
         it["removes braces for getter names"] = () => {
-            var files = generator.Generate(new[] { "MyBlueprint (Version 2)" });
-            files[0].fileContent.ToUnixLineEndings().should_be(@"using Entitas.Serialization.Blueprints;
+            generateFile(@"using Entitas.Serialization.Blueprints;
 
 namespace Entitas.Unity.Serialization.Blueprints {
     public partial class Blueprints {
         public Blueprint MyBlueprintVersion2 { get { return GetBlueprint(""MyBlueprint (Version 2)""); } }
     }
 }
-");
+", "MyBlueprint (Version 2)");
         };
     }
 }
