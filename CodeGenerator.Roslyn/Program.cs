e3610778c766a2a5a3b79434e4d4828a2e9e9cb1
Cedric Deege <cedric.deege@gmx.de>
2016-06-21 11:58:34 +0200

    Prepared integration of RoslynProvider

--- CodeGenerator.Roslyn/Program.cs
+++ CodeGenerator.Roslyn/Program.cs
@@ -3,6 +3,7 @@ using Entitas.CodeGeneration;
 using System.IO;
 using Entitas.Unity;
 using System.Reflection;
+using CSharpCodeGenerator;
 
 namespace CodeGeneration.Roslyn
 {
@@ -11,20 +12,20 @@ namespace CodeGeneration.Roslyn
         public static void Main (string [] args)
         {
             /* Properties:
-Entitas.CodeGenerator.AssemblyPath = CodeGenerator.Dependency.dll
+Entitas.CodeGenerator.ProjectPath = CodeGenerator.Dependency.dll
 Entitas.CodeGenerator.GeneratedFolderPath = ../../GeneratedTestFolderPath
-Entitas.CodeGenerator.PoolNames = meta, world
+Entitas.CodeGenerator.PoolNames = meta,world
 Entitas.CodeGenerator.BlueprintNames =
 
             */
             var propertiesContent = File.ReadAllText (args [0]);
             var properties = new Properties (propertiesContent);
-            var assemblyPath = properties ["Entitas.CodeGenerator.AssemblyPath"];
-            if (!File.Exists (assemblyPath)) {
-                throw new Exception (string.Format ("Assembly at path '{0}' does not exist!", assemblyPath));
+            var projectPath = properties ["Entitas.CodeGenerator.ProjectPath"];
+            if (!File.Exists (projectPath)) {
+                throw new Exception ($"Project at path '{projectPath}' does not exist!");
             }
 
-            var assembly = Assembly.LoadFrom (assemblyPath);
+            var project = ProjectStructure.Load(projectPath);
             var outputDirectory = properties ["Entitas.CodeGenerator.GeneratedFolderPath"];
             var pools = properties ["Entitas.CodeGenerator.PoolNames"].Split (',');
             var blueprintNames = properties ["Entitas.CodeGenerator.BlueprintNames"].Split (',');
@@ -38,7 +39,8 @@ Entitas.CodeGenerator.BlueprintNames =
             };
 
 
-            var output = RoslynCodeGenerator.Generate (assembly, pools, blueprintNames, outputDirectory, codeGenerators);
+            var output = RoslynCodeGenerator.Generate (project, pools, blueprintNames, outputDirectory, codeGenerators);
+
             foreach (var file in output) {
                 System.Console.WriteLine ("file.fileName: " + file.fileName);
             }
--- CodeGenerator.Roslyn/RoslynCodeGenerator.cs
+++ CodeGenerator.Roslyn/RoslynCodeGenerator.cs
@@ -1,20 +1,20 @@
 ﻿using System.Reflection;
+using CSharpCodeGenerator;
 
 namespace Entitas.CodeGeneration
 {
     public static class RoslynCodeGenerator
     {
-        public static CodeGenFile [] Generate (Assembly assembly, string [] poolNames, string [] blueprintNames, string directory, ICodeGenerator [] codeGenerators)
+        public static CodeGenFile [] Generate (ProjectStructure assembly, string [] poolNames, string [] blueprintNames, string directory, ICodeGenerator [] codeGenerators)
         {
             System.Console.WriteLine ("Generate...");
             // TODO create RoslynProvider
-            //var provider = new TypeReflectionProvider (assembly.GetTypes (), poolNames, blueprintNames);
-            //IPostProcessor [] postProcessors = {
-            //    new AddHeaderToFileProcessor(),
-            //    new WriteToDirectoryProcessor(directory)
-            //};
-            //return CodeGenerator.Generate (provider, codeGenerators, postProcessors);
-            return null;
+            var provider = new TypeReflectionProvider (assembly.GetTypes (), poolNames, blueprintNames);
+            IPostProcessor [] postProcessors = {
+                new AddHeaderToFileProcessor(),
+                new WriteToDirectoryProcessor(directory)
+            };
+            return CodeGenerator.Generate (provider, codeGenerators, postProcessors);
         }
     }
 }
