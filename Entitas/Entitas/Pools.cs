1b4c5ad1c0434e8c68118372da6a1d458adf067c
Simon Schmid <mail@sschmid.com>
2016-08-28 23:09:35 +0200

    Fixed Pools.CreatePool() to not create a PoolObserver in EditMode

--- Entitas/Entitas/Pools.cs
+++ Entitas/Entitas/Pools.cs
@@ -18,8 +18,10 @@
         public static Pool CreatePool(string poolName, int totalComponents, string[] componentNames, System.Type[] componentTypes) {
             var pool = new Pool(totalComponents, 0, new PoolMetaData(poolName, componentNames, componentTypes));
             #if(!ENTITAS_DISABLE_VISUAL_DEBUGGING && UNITY_EDITOR)
-            var poolObserver = new Entitas.Unity.VisualDebugging.PoolObserver(pool);
-            UnityEngine.Object.DontDestroyOnLoad(poolObserver.entitiesContainer);
+            if (UnityEngine.Application.isPlaying) {
+                var poolObserver = new Entitas.Unity.VisualDebugging.PoolObserver(pool);
+                UnityEngine.Object.DontDestroyOnLoad(poolObserver.entitiesContainer);
+            }
             #endif
 
             return pool;
