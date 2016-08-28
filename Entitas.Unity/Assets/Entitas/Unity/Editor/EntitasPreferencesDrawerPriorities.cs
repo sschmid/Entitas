a4ac9915eb22fe44ddc77d8abe9f4d169b16e0c0
Simon Schmid <mail@sschmid.com>
2016-08-28 23:16:11 +0200

    Fixed wrong namespaces

--- Entitas.Unity/Assets/Entitas/Unity/Editor/EntitasPreferencesDrawerPriorities.cs
+++ Entitas.Unity/Assets/Entitas/Unity/Editor/EntitasPreferencesDrawerPriorities.cs
@@ -1,7 +1,9 @@
-﻿public static class EntitasPreferencesDrawerPriorities {
+﻿namespace Entitas.Unity {
+    
+    public static class EntitasPreferencesDrawerPriorities {
 
-    public const int preferences = 0;
-    public const int codeGenerator = 10;
-    public const int visualDebugging = 20;
+        public const int preferences = 0;
+        public const int codeGenerator = 10;
+        public const int visualDebugging = 20;
+    }
 }
-
