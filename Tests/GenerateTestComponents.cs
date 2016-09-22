78dde3062484bd875bc15559624bebd82ed9b51b
Simon Schmid <mail@sschmid.com>
2016-09-23 00:30:48 +0200

    Added TestRunner to support test debugging

--- Tests/GenerateTestComponents.cs
+++ Tests/GenerateTestComponents.cs
@@ -1,44 +1,46 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using Entitas.CodeGenerator;
+﻿//using System;
+//using System.IO;
+//using System.Reflection;
+//using Entitas.CodeGenerator;
 
-class GenerateTestComponents {
+//namespace Tests {
 
-    public static void Main(string[] args) {
-        generate();
-    }
+//    class GenerateTestComponents {
 
-    static void generate() {
-        var assembly = Assembly.GetAssembly(typeof(SomeComponent));
-        var generatedFolder = getEntitasProjectDir() + "/Tests/Tests/Entitas/CodeGenerator/Fixtures/Generated/";
+//        public static void Main(string[] args) {
+//            generate();
+//        }
 
-        var codeGenerators = new ICodeGenerator[] {
-            new ComponentExtensionsGenerator(),
-            new ComponentIndicesGenerator(),
-            new PoolAttributesGenerator(),
-            new PoolsGenerator()
-        };
+//        static void generate() {
+//            var assembly = Assembly.GetAssembly(typeof(SomeComponent));
+//            var generatedFolder = getEntitasProjectDir() + "/Tests/Tests/Entitas/CodeGenerator/Fixtures/Generated/";
 
-        var provider = new TypeReflectionProvider(assembly.GetTypes(), new string[0], new string[0]);
-        var files = CodeGenerator.Generate(provider, generatedFolder, codeGenerators);
+//            var codeGenerators = new ICodeGenerator[] {
+//            new ComponentExtensionsGenerator(),
+//            new ComponentIndicesGenerator(),
+//            new PoolAttributesGenerator(),
+//            new PoolsGenerator()
+//        };
 
-        foreach (var file in files) {
-            Console.WriteLine("Generated: " + file.fileName);
-        }
+//            var provider = new TypeReflectionProvider(assembly.GetTypes(), new string[0], new string[0]);
+//            var files = CodeGenerator.Generate(provider, generatedFolder, codeGenerators);
 
-        Console.WriteLine("Done. Press any key...");
-        Console.Read();
-    }
+//            foreach(var file in files) {
+//                Console.WriteLine("Generated: " + file.fileName);
+//            }
 
-    static string getEntitasProjectDir() {
-        var dirInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
-        const string projectName = "Tests";
-        while (dirInfo.Name != projectName) {
-            dirInfo = dirInfo.Parent;
-        }
+//            Console.WriteLine("Done. Press any key...");
+//            Console.Read();
+//        }
 
-        return dirInfo.Parent.FullName;
-    }
-}
+//        static string getEntitasProjectDir() {
+//            var dirInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+//            const string projectName = "Tests";
+//            while(dirInfo.Name != projectName) {
+//                dirInfo = dirInfo.Parent;
+//            }
 
+//            return dirInfo.Parent.FullName;
+//        }
+//    }
+//}
