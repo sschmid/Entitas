c4305245c6fe959cddbc210032774db0838b77c4
Simon Schmid <mail@sschmid.com>
2016-09-24 10:20:45 +0200

    Added describe_EntityExtension for existing EntityExtension

---
+++ Tests/Tests/Entitas/describe_EntityExtension.cs
@@ -0,0 +1,34 @@
+﻿using NSpec;
+using Entitas;
+
+class describe_EntityExtension : nspec {
+
+    void when_entity() {
+
+        context["when adding ComponentSuffix"] = () => {
+
+            it["doesn't add component suffix to string ending with ComponentSuffix"] = () => {
+                const string str = "Position" + EntityExtension.COMPONENT_SUFFIX;
+                str.AddComponentSuffix().should_be_same(str);
+            };
+
+            it["add ComponentSuffix to string not ending with ComponentSuffix"] = () => {
+                const string str = "Position";
+                str.AddComponentSuffix().should_be("Position" + EntityExtension.COMPONENT_SUFFIX);
+            };
+        };
+
+        context["when removeing ComponentSuffix"] = () => {
+
+            it["doesn't change string when not ending with ComponentSuffix"] = () => {
+                const string str = "Position";
+                str.RemoveComponentSuffix().should_be_same(str);
+            };
+
+            it["removes ComponentSuffix when ending with ComponentSuffix"] = () => {
+                const string str = "Position" + EntityExtension.COMPONENT_SUFFIX;
+                str.RemoveComponentSuffix().should_be("Position");
+            };
+        };
+    }
+}
