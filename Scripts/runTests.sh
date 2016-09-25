6509df897e59bf0205ed6f2b176d6ce0f7761614
Simon Schmid <mail@sschmid.com>
2016-09-25 19:10:25 +0200

    Removed logging paths

--- Scripts/runTests.sh
+++ Scripts/runTests.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 source Scripts/vars.sh
-
-echo `pwd`
-
-echo "Building $TEST_PROJECT"
-
 xbuild $TEST_PROJECT /property:Configuration=Release /verbosity:minimal
 if [ $? = 0 ]
 then
-	echo "Running tests $TEST_RUNNER"
 	mono $TEST_RUNNER
 else
 	echo "ERROR: Could not compile!"
