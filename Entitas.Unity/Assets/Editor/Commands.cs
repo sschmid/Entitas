fa40b50864049972748846b59a080f5e7cfce4ed
Simon Schmid <mail@sschmid.com>
2016-10-03 00:14:45 +0200

    Updated complete build pipeline

--- Entitas.Unity/Assets/Editor/Commands.cs
+++ Entitas.Unity/Assets/Editor/Commands.cs
@@ -2,7 +2,7 @@
 
 public static class Commands {
 
-    public static void GenerateProjectFiles() {
+    public static void GenerateCSharpProject() {
         EditorApplication.ExecuteMenuItem("Assets/Open C# Project");
     }
 }
--- Scripts/update_project_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-source Scripts/build_commands.sh
-
-collect_sources
-update_project_dependencies
