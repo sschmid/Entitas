91a5ad14da71e47f47795083371f9aeb3caae3bc
Simon Schmid <mail@sschmid.com>
2017-01-06 20:54:21 +0100

    Renamed pool related code in Contexts

--- Entitas/Entitas/Contexts.cs
+++ Entitas/Entitas/Contexts.cs
@@ -15,22 +15,21 @@ namespace Entitas {
 
         static Contexts _sharedInstance;
 
-        public static Context CreatePool(string poolName,
+        public static Context CreateContext(string name,
                                       int totalComponents,
                                       string[] componentNames,
                                       System.Type[] componentTypes) {
-            var pool = new Context(totalComponents, 0, new ContextInfo(
-                poolName, componentNames, componentTypes)
+            var context = new Context(totalComponents, 0, new ContextInfo(
+                name, componentNames, componentTypes)
             );
 #if(!ENTITAS_DISABLE_VISUAL_DEBUGGING && UNITY_EDITOR)
             if(UnityEngine.Application.isPlaying) {
-                var poolObserver =
-                    new Entitas.Unity.VisualDebugging.PoolObserver(pool);
-                UnityEngine.Object.DontDestroyOnLoad(poolObserver.gameObject);
+                var observer = new Entitas.Unity.VisualDebugging.ContextObserver(context);
+                UnityEngine.Object.DontDestroyOnLoad(observer.gameObject);
             }
 #endif
 
-            return pool;
+            return context;
         }
     }
 }
