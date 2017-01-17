ded5b2d233f9d910279224ce9e287dbf8a3bee9d
Simon Schmid <mail@sschmid.com>
2017-01-17 18:40:40 +0100

    Group maintenance

--- Entitas/Entitas/Group/Group.cs
+++ Entitas/Entitas/Group/Group.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Entitas {
 
@@ -182,18 +181,6 @@ namespace Entitas {
                 _toStringCache = "Group(" + _matcher + ")";
             }
             return _toStringCache;
-        }
-    }
-
-    public class GroupSingleEntityException<TEntity> : EntitasException where TEntity : class, IEntity, new() {
-        public GroupSingleEntityException(IGroup<TEntity> group) :
-            base(
-                "Cannot get the single entity from " + group +
-                "!\nGroup contains " + group.count + " entities:",
-                string.Join("\n",
-                    group.GetEntities().Select(e => e.ToString()).ToArray()
-                )
-            ) {
         }
     }
 }
---
+++ Entitas/Entitas/Group/GroupSingleEntityException.cs
@@ -0,0 +1,13 @@
+using System.Linq;
+
+namespace Entitas {
+
+    public class GroupSingleEntityException<TEntity> : EntitasException where TEntity : class, IEntity, new() {
+
+        public GroupSingleEntityException(IGroup<TEntity> group)
+            : base("Cannot get the single entity from " + group +
+                "!\nGroup contains " + group.count + " entities:",
+                string.Join("\n", group.GetEntities().Select(e => e.ToString()).ToArray())) {
+        }
+    }
+}
