d7c259dac5f6f7c219c12b248a699bfe583e54c0
Simon Schmid <mail@sschmid.com>
2017-03-18 17:59:37 +0100

    Made EntitasPreferences.CONFIG_PATH writeable. Closes #342

--- Entitas/Entitas/Utils/Configuration/EntitasPreferences.cs
+++ Entitas/Entitas/Utils/Configuration/EntitasPreferences.cs
@@ -4,7 +4,7 @@ namespace Entitas {
 
     public static class EntitasPreferences {
 
-        public const string CONFIG_PATH = "Entitas.properties";
+        public static string CONFIG_PATH = "Entitas.properties";
 
         public static EntitasPreferencesConfig LoadConfig() {
             return new EntitasPreferencesConfig(File.Exists(CONFIG_PATH) ? File.ReadAllText(CONFIG_PATH) : string.Empty);
