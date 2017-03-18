4287f308531d082686ecc2b709a8e09849859f9b
Simon Schmid <mail@sschmid.com>
2017-03-18 17:58:43 +0100

    Added ConsoleWriteLinePostProcessor #342

---
+++ Addons/Entitas.CodeGenerator/Entitas.CodeGenerator/PostProcessors/ConsoleWriteLinePostProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Entitas.CodeGenerator {
+
+    public class ConsoleWriteLinePostProcessor : ICodeGenFilePostProcessor {
+
+        public string name { get { return "Console.WriteLine generated files"; } }
+        public bool isEnabledByDefault { get { return false; } }
+        public int priority { get { return 200; } }
+
+        public CodeGenFile[] PostProcess(CodeGenFile[] files) {
+            foreach(var file in files) {
+                Console.WriteLine(file.generatorName + ": " + file.fileName);
+            }
+
+            return files;
+        }
+    }
+}
