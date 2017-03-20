13938b4e7747b35fe73732fd9aa97c220b2eea57
Simon Schmid <mail@sschmid.com>
2017-03-20 16:18:58 +0100

    Updated describe_ComponentDataProvider

--- Tests/Tests/Tests/Entitas.CodeGenerator/DataProviders/describe_ComponentDataProvider.cs
+++ Tests/Tests/Tests/Entitas.CodeGenerator/DataProviders/describe_ComponentDataProvider.cs
@@ -1,224 +1,181 @@
-//using System;
-//using Entitas.CodeGenerator;
-//using NSpec;
-//using My.Namespace;
-//using Entitas;
+using System;
+using Entitas.CodeGenerator;
+using NSpec;
+using My.Namespace;
+using Entitas;
 
-//class describe_ComponentDataProvider : nspec {
+class describe_ComponentDataProvider : nspec {
 
-//    ComponentData getData<T>() {
-//        var provider = new ComponentDataProvider(new Type[] { typeof(T) });
-//        return (ComponentData)provider.GetData()[0];
-//    }
+    ComponentData getData<T>() {
+        return getMultipleData<T>()[0];
+    }
 
-//    void when_providing() {
+    ComponentData[] getMultipleData<T>() {
+        var provider = new ComponentDataProvider(new Type[] { typeof(T) });
+        return (ComponentData[])provider.GetData();
+    }
 
-//        context["component"] = () => {
+    void when_providing() {
 
-//            Type[] types = null;
-//            ComponentData[] data = null;
-//            ComponentData d = null;
+        context["component"] = () => {
 
-//            before = () => {
-//                types = new [] { typeof(MyNamespaceComponent) };
-//                var provider = new ComponentDataProvider(types);
-//                data = (ComponentData[])provider.GetData();
-//                d = data[0];
-//            };
+            Type type = null;
+            ComponentData data = null;
 
-//            it["get data"] = () => {
-//                data.Length.should_be(1);
-//            };
+            before = () => {
+                type = typeof(MyNamespaceComponent);
+                data = getData<MyNamespaceComponent>();
+            };
 
-//            it["gets component name"] = () => {
-//                d.GetComponentName().GetType().should_be(typeof(string));
-//                d.GetComponentName().should_be("MyNamespaceMyNamespace");
+            it["get data"] = () => {
+                data.should_not_be_null();
+            };
 
-//                d.GetFullComponentName().GetType().should_be(typeof(string));
-//                d.GetFullComponentName().should_be("MyNamespaceMyNamespaceComponent");
-//            };
+            it["gets full type name"] = () => {
+                data.GetFullTypeName().GetType().should_be(typeof(string));
+                data.GetFullTypeName().should_be(type.ToCompilableString());
+            };
 
-//            it["gets full type name"] = () => {
-//                d.GetFullTypeName().GetType().should_be(typeof(string));
-//                d.GetFullTypeName().should_be(types[0].ToCompilableString());
-//            };
+            it["gets contexts"] = () => {
+                var contextNames = data.GetContextNames();
+                contextNames.GetType().should_be(typeof(string[]));
+                contextNames.Length.should_be(2);
+                contextNames[0].should_be("Test");
+                contextNames[1].should_be("Test2");
+            };
 
-//            it["gets contexts"] = () => {
-//                d.GetContextNames().GetType().should_be(typeof(string[]));
-//                d.GetContextNames().Length.should_be(2);
-//                d.GetContextNames()[0].should_be("Test");
-//                d.GetContextNames()[1].should_be("Test2");
-//            };
+            it["sets first context as default when component has no context"] = () => {
+                var contextNames = getData<NoContextComponent>().GetContextNames();
+                contextNames.Length.should_be(1);
+                contextNames[0].should_be("Game");
+            };
 
-//            it["sets first context as default when component has no context"] = () => {
-//                var contextNames = getData<NoContextComponent>().GetContextNames();
-//                contextNames.Length.should_be(1);
-//                contextNames[0].should_be("Game");
-//            };
+            it["gets unique"] = () => {
+                data.IsUnique().GetType().should_be(typeof(bool));
+                data.IsUnique().should_be_false();
 
-//            it["gets unique"] = () => {
-//                d.IsUnique().GetType().should_be(typeof(bool));
-//                d.IsUnique().should_be_false();
+                getData<UniqueStandardComponent>().IsUnique().should_be_true();
+            };
 
-//                getData<UniqueStandardComponent>().IsUnique().should_be_true();
-//            };
+            it["gets member data"] = () => {
+                data.GetMemberData().GetType().should_be(typeof(MemberData[]));
+                data.GetMemberData().Length.should_be(1);
+                data.GetMemberData()[0].type.should_be("string");
+            };
 
-//            it["gets member data"] = () => {
-//                d.GetMemberData().GetType().should_be(typeof(MemberData[]));
-//                d.GetMemberData().Length.should_be(1);
-//            };
+            it["gets generate component"] = () => {
+                data.ShouldGenerateComponent().GetType().should_be(typeof(bool));
+                data.ShouldGenerateComponent().should_be_false();
+                data.ContainsKey(ShouldGenerateComponentComponentDataExtension.COMPONENT_OBJECT_TYPE).should_be_false();
+            };
 
-//            it["gets generate component"] = () => {
-//                d.ShouldGenerateComponent().GetType().should_be(typeof(bool));
-//                d.ShouldGenerateComponent().should_be_false();
-//                d.ContainsKey(ShouldGenerateComponentComponentDataExtension.COMPONENT_OBJECT_TYPE).should_be_false();
-//            };
+            it["gets generate index"] = () => {
+                data.ShouldGenerateIndex().GetType().should_be(typeof(bool));
+                data.ShouldGenerateIndex().should_be_true();
 
-//            it["gets generate index"] = () => {
-//                d.ShouldGenerateIndex().GetType().should_be(typeof(bool));
-//                d.ShouldGenerateIndex().should_be_true();
+                getData<DontGenerateIndexComponent>().ShouldGenerateIndex().should_be_false();
+            };
 
-//                getData<DontGenerateIndexComponent>().ShouldGenerateIndex().should_be_false();
-//            };
+            it["gets generate methods"] = () => {
+                data.ShouldGenerateMethods().GetType().should_be(typeof(bool));
+                data.ShouldGenerateMethods().should_be_true();
 
-//            it["gets generate methods"] = () => {
-//                d.ShouldGenerateMethods().GetType().should_be(typeof(bool));
-//                d.ShouldGenerateMethods().should_be_true();
+                getData<DontGenerateMethodsComponent>().ShouldGenerateMethods().should_be_false();
+            };
 
-//                getData<DontGenerateMethodsComponent>().ShouldGenerateMethods().should_be_false();
-//            };
+            it["gets unique prefix"] = () => {
+                data.GetUniqueComponentPrefix().GetType().should_be(typeof(string));
+                data.GetUniqueComponentPrefix().should_be("is");
 
-//            it["gets unique prefix"] = () => {
-//                d.GetUniqueComponentPrefix().GetType().should_be(typeof(string));
-//                d.GetUniqueComponentPrefix().should_be("is");
+                getData<CustomPrefixFlagComponent>().GetUniqueComponentPrefix().should_be("My");
+            };
+        };
 
-//                getData<CustomPrefixFlagComponent>().GetUniqueComponentPrefix().should_be("My");
-//            };
-//        };
+        context["non component"] = () => {
 
-//        context["non component"] = () => {
+            Type type = null;
+            ComponentData data = null;
 
-//            Type[] types = null;
-//            ComponentData[] data = null;
-//            ComponentData d = null;
+            before = () => {
+                type = typeof(ClassToGenerate);
+                data = getData<ClassToGenerate>();
+            };
 
-//            before = () => {
-//                types = new [] { typeof(ClassToGenerate) };
-//                var provider = new ComponentDataProvider(types);
-//                data = (ComponentData[])provider.GetData();
-//                d = data[0];
-//            };
+            it["get data"] = () => {
+                data.should_not_be_null();
+            };
 
-//            it["get data"] = () => {
-//                data.Length.should_be(1);
-//            };
+            it["gets full type name"] = () => {
+                // Not the type, but the component that should be generated
+                // See: no namespace
+                data.GetFullTypeName().should_be("ClassToGenerateComponent");
+            };
 
-//            it["gets component name"] = () => {
-//                d.GetComponentName().GetType().should_be(typeof(string));
+            it["gets contexts"] = () => {
+                var contextNames = data.GetContextNames();
+                contextNames.Length.should_be(2);
+                contextNames[0].should_be("Test");
+                contextNames[1].should_be("Test2");
+            };
 
-//                // Not the type, but the component that should be generated
-//                // See: no namespace
-//                d.GetComponentName().should_be("ClassToGenerate");
+            it["gets unique"] = () => {
+                data.IsUnique().should_be_false();
+            };
 
-//                d.GetFullComponentName().GetType().should_be(typeof(string));
+            it["gets member data"] = () => {
+                data.GetMemberData().Length.should_be(1);
+                data.GetMemberData()[0].type.should_be(type.ToCompilableString());
+            };
 
-//                // Not the type, but the component that should be generated
-//                // See: no namespace
-//                d.GetFullComponentName().should_be("ClassToGenerateComponent");
-//            };
+            it["gets generate component"] = () => {
+                data.ShouldGenerateComponent().GetType().should_be(typeof(bool));
+                data.ShouldGenerateComponent().should_be_true();
+                data.GetObjectType().should_be(typeof(ClassToGenerate).ToCompilableString());
+            };
 
-//            it["gets full type name"] = () => {
-//                d.GetFullTypeName().GetType().should_be(typeof(string));
+            it["gets generate index"] = () => {
+                data.ShouldGenerateIndex().should_be_true();
+            };
 
-//                // Not the type, but the component that should be generated
-//                // See: no namespace
-//                d.GetFullTypeName().should_be("ClassToGenerateComponent");
-//            };
+            it["gets generate methods"] = () => {
+                data.ShouldGenerateMethods().should_be_true();
+            };
 
-//            it["gets contexts"] = () => {
-//                d.GetContextNames().GetType().should_be(typeof(string[]));
-//                d.GetContextNames().Length.should_be(2);
-//                d.GetContextNames()[0].should_be("Test");
-//                d.GetContextNames()[1].should_be("Test2");
-//            };
+            it["gets unique prefix"] = () => {
+                data.GetUniqueComponentPrefix().should_be("is");
+            };
+        };
 
-//            it["gets unique"] = () => {
-//                d.IsUnique().GetType().should_be(typeof(bool));
-//                d.IsUnique().should_be_false();
-//            };
+        context["multiple types"] = () => {
 
-//            it["gets member data"] = () => {
-//                d.GetMemberData().Length.should_be(1);
-//                d.GetMemberData()[0].type.should_be(typeof(ClassToGenerate).ToCompilableString());
-//            };
+            it["creates data for each type"] = () => {
+                var types = new [] { typeof(NameAgeComponent), typeof(Test2ContextComponent) };
+                var provider = new ComponentDataProvider(types);
+                var data = provider.GetData();
+                data.Length.should_be(types.Length);
+            };
+        };
 
-//            it["gets generate component"] = () => {
-//                d.ShouldGenerateComponent().GetType().should_be(typeof(bool));
-//                d.ShouldGenerateComponent().should_be_true();
-//                d.GetObjectType().should_be(typeof(ClassToGenerate).ToCompilableString());
-//            };
+        context["multiple custom component names"] = () => {
 
-//            it["gets generate index"] = () => {
-//                d.ShouldGenerateIndex().GetType().should_be(typeof(bool));
-//                d.ShouldGenerateIndex().should_be_true();
-//            };
+            Type type = null;
+            ComponentData data1 = null;
+            ComponentData data2 = null;
 
-//            it["gets generate methods"] = () => {
-//                d.ShouldGenerateMethods().GetType().should_be(typeof(bool));
-//                d.ShouldGenerateMethods().should_be_true();
-//            };
+            before = () => {
+                type = typeof(CustomName);
+                var data = getMultipleData<CustomName>();
+                data1 = data[0];
+                data2 = data[1];
+            };
 
-//            it["gets unique prefix"] = () => {
-//                d.GetUniqueComponentPrefix().GetType().should_be(typeof(string));
-//                d.GetUniqueComponentPrefix().should_be("is");
-//            };
-//        };
+            it["creates data for each custom component name"] = () => {
+                data1.GetObjectType().should_be(type.ToCompilableString());
+                data2.GetObjectType().should_be(type.ToCompilableString());
 
-//        context["multiple types"] = () => {
-
-//            it["creates data for each type"] = () => {
-//                var types = new [] { typeof(NameAgeComponent), typeof(Test2ContextComponent) };
-//                var provider = new ComponentDataProvider(types);
-//                var data = provider.GetData();
-//                data.Length.should_be(types.Length);
-//            };
-//        };
-
-//        context["multiple custom component names"] = () => {
-
-//            Type[] types = null;
-//            ComponentData[] data = null;
-//            ComponentData d1 = null;
-//            ComponentData d2 = null;
-
-//            before = () => {
-//                types = new [] { typeof(CustomName) };
-//                var provider = new ComponentDataProvider(types);
-//                data = (ComponentData[])provider.GetData();
-//                d1 = data[0];
-//                d2 = data[1];
-//            };
-
-//            it["get data"] = () => {
-//                data.Length.should_be(2);
-//            };
-
-//            it["creates data for each custom component name"] = () => {
-//                d1.GetComponentName().should_be("NewCustomNameComponent1");
-//                d2.GetComponentName().should_be("NewCustomNameComponent2");
-
-//                d1.GetObjectType().should_be(types[0].ToCompilableString());
-//                d2.GetObjectType().should_be(types[0].ToCompilableString());
-
-//                d1.GetFullTypeName().should_be("NewCustomNameComponent1Component");
-//                d2.GetFullTypeName().should_be("NewCustomNameComponent2Component");
-
-//                d1.GetComponentName().should_be("NewCustomNameComponent1");
-//                d2.GetComponentName().should_be("NewCustomNameComponent2");
-
-//                d1.GetFullComponentName().should_be("NewCustomNameComponent1Component");
-//                d2.GetFullComponentName().should_be("NewCustomNameComponent2Component");
-//            };
-//        };
-//    }
-//}
+                data1.GetFullTypeName().should_be("NewCustomNameComponent1Component");
+                data2.GetFullTypeName().should_be("NewCustomNameComponent2Component");
+            };
+        };
+    }
+}
