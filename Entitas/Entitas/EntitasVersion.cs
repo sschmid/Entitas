b64a92ff696871c9fc0b9d7a4a7b3ba4682ba862
Simon Schmid <mail@sschmid.com>
2017-03-27 14:11:14 +0200

    Added EntitasVersion

---
+++ Entitas/Entitas/EntitasVersion.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Entitas {
+
+    public static class EntitasVersion {
+
+        public static string GetVersion() {
+            var assembly = typeof(Entity).Assembly;
+            var stream = assembly.GetManifestResourceStream("version");
+            var version = "";
+            using(var reader = new StreamReader(stream)) {
+                version = reader.ReadToEnd();
+            }
+
+            return version;
+        }
+    }
+}
---
+++ Tests/Tests/Tests/Entitas/describe_EntitasVersion.cs
@@ -0,0 +1,12 @@
+﻿using Entitas;
+using NSpec;
+
+class describe_EntitasVersion : nspec {
+
+    void when_version() {
+
+        xit["gets version"] = () => {
+            EntitasVersion.GetVersion().should_be("0.40.0");
+        };
+    }
+}
