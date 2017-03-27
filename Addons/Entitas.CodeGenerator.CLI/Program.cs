5c62b1668a5f43fa14821d219eaf09e13d800cb7
Simon Schmid <mail@sschmid.com>
2017-03-27 13:54:41 +0200

    Added Entitas.CodeGenerator.CLI

--- Addons/Entitas.CodeGenerator.CLI/Program.cs
+++ Addons/Entitas.CodeGenerator.CLI/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using Fabl;
 
-namespace Entitas.CodeGenerator {
+namespace Entitas.CodeGenerator.CLI {
 
     class MainClass {
 
--- Addons/Entitas.CodeGenerator/Entitas.CodeGenerator/DependencyResolver.cs
+++ Addons/Entitas.CodeGenerator/Entitas.CodeGenerator/DependencyResolver.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using Fabl;
 using System.Collections.Generic;
