17b95baf4bbcfc117bde216fc19f035a8df758a5
Simon Schmid <mail@sschmid.com>
2017-04-04 16:27:22 +0200

    Showing potential exception in PreferencesWindow

--- Entitas.Utils/Utils/Configuration/Properties.cs
+++ Entitas.Utils/Utils/Configuration/Properties.cs
@@ -114,7 +114,7 @@ namespace Entitas {
 
         public readonly string key;
 
-        public InvalidKeyPropertiesException(string key) : base("Invalid key " + key) {
+        public InvalidKeyPropertiesException(string key) : base("Invalid key: " + key) {
             this.key = key;
         }
     }
