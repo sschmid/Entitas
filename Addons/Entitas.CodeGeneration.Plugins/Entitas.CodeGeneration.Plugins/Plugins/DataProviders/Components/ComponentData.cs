b5b91fe3c2f186fd3493642a1fcab369059bd573
Simon Schmid <mail@sschmid.com>
2017-04-09 20:22:16 +0200

    Added config key to ignore namespaces

--- Addons/Entitas.CodeGeneration.Plugins/Entitas.CodeGeneration.Plugins/Plugins/DataProviders/Components/ComponentData.cs
+++ Addons/Entitas.CodeGeneration.Plugins/Entitas.CodeGeneration.Plugins/Plugins/DataProviders/Components/ComponentData.cs
@@ -8,7 +8,21 @@ namespace Entitas.CodeGeneration.Plugins {
     public static class ComponentDataExtension {
 
         public static string ToComponentName(this string fullTypeName) {
-            return fullTypeName.RemoveDots().RemoveComponentSuffix();
+            return shouldIgnoreNamespaces()
+                ? fullTypeName.ShortTypeName().RemoveComponentSuffix()
+                : fullTypeName.RemoveDots().RemoveComponentSuffix();
+        }
+
+        public const string IGNORE_NAMESPACES_KEY = "Entitas.CodeGeneration.Plugins.IgnoreNamespaces";
+
+        static Config _config;
+
+        static bool shouldIgnoreNamespaces() {
+            if(_config == null) {
+                _config = Preferences.LoadConfig();
+            }
+
+            return _config.GetValueOrDefault(IGNORE_NAMESPACES_KEY, "false") == "true";
         }
     }
 }
