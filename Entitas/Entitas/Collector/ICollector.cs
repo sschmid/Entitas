490918836accf5235d55815511b47cb65a74785c
Simon Schmid <mail@sschmid.com>
2017-04-24 14:46:58 +0200

    Added ICollector

---
+++ Entitas/Entitas/Collector/ICollector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Entitas {
+
+    public interface ICollector {
+
+		void Activate();
+		void Deactivate();
+		void ClearCollectedEntities();
+    }
+
+    public interface ICollector<TEntity> : ICollector where TEntity : class, IEntity {
+
+        HashSet<TEntity> collectedEntities { get; }
+    }
+}
