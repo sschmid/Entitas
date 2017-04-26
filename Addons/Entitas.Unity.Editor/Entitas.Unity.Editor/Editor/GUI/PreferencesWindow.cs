0e704a670d9de6480ddac08b4889f69e24adbf77
Simon Schmid <mail@sschmid.com>
2017-04-26 10:24:36 +0200

    Removed Donate Button

--- Addons/Entitas.Unity.Editor/Entitas.Unity.Editor/Editor/GUI/PreferencesWindow.cs
+++ Addons/Entitas.Unity.Editor/Entitas.Unity.Editor/Editor/GUI/PreferencesWindow.cs
@@ -71,9 +71,9 @@ namespace Entitas.Unity.Editor {
                 if (GUILayout.Button("Wiki", EditorStyles.toolbarButton)) {
                     EntitasFeedback.EntitasWiki();
                 }
-                if (GUILayout.Button("Donate", EditorStyles.toolbarButton)) {
-                    EntitasFeedback.Donate();
-                }
+                //if (GUILayout.Button("Donate", EditorStyles.toolbarButton)) {
+                //    EntitasFeedback.Donate();
+                //}
             }
             EditorGUILayout.EndHorizontal();
         }
