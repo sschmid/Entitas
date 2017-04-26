870e488aa026f26f52ecc3bb2a0f1f1bbe28ff10
Simon Schmid <mail@sschmid.com>
2017-04-26 10:03:22 +0200

    Added EntityAccessAttribute

---
+++ Addons/Entitas.CodeGeneration.Attributes/Entitas.CodeGeneration.Attributes/Attributes/EntityAccess/EntityAccessAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Entitas.CodeGeneration.Attributes {
+
+    public class AbstractEntityAccessAttribute : Attribute {
+
+        public readonly EntityAccessType entityAccess;
+        public readonly Type componentType;
+
+        protected AbstractEntityAccessAttribute(EntityAccessType entityAccess,  Type componentType) {
+            this.entityAccess = entityAccess;
+            this.componentType = componentType;;
+        }
+    }
+}
---
+++ Addons/Entitas.CodeGeneration.Attributes/Entitas.CodeGeneration.Attributes/Attributes/EntityAccess/WriteAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entitas.CodeGeneration.Attributes {
+
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct)]
+    public class WriteAttribute : AbstractEntityAccessAttribute {
+
+        public WriteAttribute(Type componentType) : base(EntityAccessType.Write, componentType) {
+        }
+    }
+}
