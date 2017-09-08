9c86611cebaac57d0e29a852b25dfd7a44a6b762
Adi Zhavo <adi@gram.gs>
2017-09-08 13:28:56 +0100

    Add test cases for resolving dependencies for systems

---
+++ Tests/Tests/Fixtures/Systems/HasDependencySystemSpy.cs
@@ -0,0 +1,20 @@
+﻿using Entitas;
+
+public class HasDependencySystemSpy :
+    // Can implement any ISystem interface
+    IHasSystemDependency<InitializeSystemSpy>,
+    IHasSystemDependency<TearDownSystemSpy> {
+
+    public int teardownSystemDeps { private set; get; }
+    public int initialiseSystemDeps { private set; get; }
+
+    public void SetSystem(InitializeSystemSpy dependency)
+    {
+        initialiseSystemDeps ++;
+    }
+    
+    public void SetSystem(TearDownSystemSpy dependency)
+    {
+        teardownSystemDeps ++;
+    }
+}
