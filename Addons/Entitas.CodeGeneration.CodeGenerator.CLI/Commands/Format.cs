ed45afc3bf5bc35f9e9028bc9140b7a65fbdc7c7
Simon Schmid <mail@sschmid.com>
2017-10-31 12:57:40 +0100

    Added format command

---
+++ Addons/Entitas.CodeGeneration.CodeGenerator.CLI/Commands/Format.cs
@@ -0,0 +1,15 @@
+﻿namespace Entitas.CodeGeneration.CodeGenerator.CLI {
+
+    public class Format : AbstractCommand {
+
+        public override string trigger { get { return "format"; } }
+        public override string description { get { return "Formats the properties file"; } }
+        public override string example { get { return "entitas format"; } }
+
+        public override void Run(string[] args) {
+            if (assertPreferences()) {
+                loadPreferences().Save();
+            }
+        }
+    }
+}
