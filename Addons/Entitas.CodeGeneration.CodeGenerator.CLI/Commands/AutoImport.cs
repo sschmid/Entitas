4a9e04e4748708f6e0295a519094563f94c1f465
Simon Schmid <mail@sschmid.com>
2017-11-09 14:39:06 +0100

    Added auto-import command

---
+++ Addons/Entitas.CodeGeneration.CodeGenerator.CLI/Commands/AutoImport.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Linq;
+using Entitas.Utils;
+using Fabl;
+
+namespace Entitas.CodeGeneration.CodeGenerator.CLI {
+
+    public class AutoImport : AbstractCommand {
+
+        public override string trigger { get { return "auto-import"; } }
+        public override string description { get { return "Find and import all plugins"; } }
+        public override string example { get { return "entitas auto-import"; } }
+
+        public override void Run(string[] args) {
+            if (assertPreferences(args)) {
+                var preferences = loadPreferences(args);
+                fabl.Debug(preferences.ToString());
+                autoImport(preferences);
+                new FixConfig().Run(args);
+            }
+        }
+
+        static void autoImport(Preferences preferences) {
+            var config = new CodeGeneratorConfig();
+            config.Configure(preferences);
+
+            var plugins = config.searchPaths
+                .SelectMany(path => Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories))
+                .Where(path => path.ToLower().EndsWith(".plugins.dll"));
+
+            config.searchPaths = config.searchPaths
+                .Concat(plugins.Select(path => Path.GetDirectoryName(path)))
+                .Distinct()
+                .ToArray();
+
+            config.plugins = plugins
+                .Select(path => Path.GetFileNameWithoutExtension(path))
+                .Distinct()
+                .ToArray();
+
+            preferences.Save();
+        }
+    }
+}
