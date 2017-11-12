66b53f8e2d55bc2f6f8f4f080907517df1e23422
Simon Schmid <mail@sschmid.com>
2017-11-12 19:35:46 +0100

    Added migration 0450

---
+++ Addons/Entitas.Migration/Entitas.Migration/Migrations/M0450.cs
@@ -0,0 +1,28 @@
+﻿namespace Entitas.Migration {
+
+    public class M0450 : IMigration {
+
+        public string version { get { return "0.45.0"; } }
+
+        public string workingDirectory { get { return "project root"; } }
+
+        public string description { get { return "Updates Entitas.properties to use renamed keys"; } }
+
+        public MigrationFile[] Migrate(string path) {
+            var properties = MigrationUtils.GetFiles(path, "Entitas.properties");
+
+            for (int i = 0; i < properties.Length; i++) {
+                var file = properties[i];
+
+                file.fileContent = file.fileContent.Replace("Entitas.CodeGeneration.CodeGenerator.SearchPaths", "CodeGenerator.SearchPaths");
+                file.fileContent = file.fileContent.Replace("Entitas.CodeGeneration.CodeGenerator.Plugins", "CodeGenerator.Plugins");
+
+                file.fileContent = file.fileContent.Replace("Entitas.CodeGeneration.CodeGenerator.DataProviders", "CodeGenerator.DataProviders");
+                file.fileContent = file.fileContent.Replace("Entitas.CodeGeneration.CodeGenerator.CodeGenerators", "CodeGenerator.CodeGenerators");
+                file.fileContent = file.fileContent.Replace("Entitas.CodeGeneration.CodeGenerator.PostProcessors", "CodeGenerator.PostProcessors");
+            }
+
+            return properties;
+        }
+    }
+}
---
+++ Tests/Tests/Tests/Entitas.Migration/describe_M0450.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Linq;
+using Entitas.Migration;
+using NSpec;
+
+class describe_M0450 : nspec {
+
+    void when_migrating() {
+
+        var dir = TestExtensions.GetProjectRoot() + "/Tests/Tests/Tests/Entitas.Migration/Fixtures/M0450";
+
+        IMigration m = null;
+
+        before = () => {
+            m = new M0450();
+        };
+
+        it["finds all files"] = () => {
+            var updatedFiles = m.Migrate(dir);
+            updatedFiles.Length.should_be(1);
+            updatedFiles.Any(file => file.fileName == Path.Combine(dir, "Entitas.properties")).should_be_true();
+        };
+
+        it["updates Entitas.properties"] = () => {
+            var updatedFiles = m.Migrate(dir);
+            var file = updatedFiles[0];
+
+            file.fileContent.Contains("Entitas.CodeGeneration.CodeGenerator.SearchPaths").should_be_false();
+            file.fileContent.Contains("CodeGenerator.SearchPaths").should_be_true();
+
+            file.fileContent.Contains("Entitas.CodeGeneration.CodeGenerator.Plugins").should_be_false();
+            file.fileContent.Contains("CodeGenerator.Plugins").should_be_true();
+
+            file.fileContent.Contains("Entitas.CodeGeneration.CodeGenerator.DataProviders").should_be_false();
+            file.fileContent.Contains("CodeGenerator.DataProviders").should_be_true();
+
+            file.fileContent.Contains("Entitas.CodeGeneration.CodeGenerator.CodeGenerators").should_be_false();
+            file.fileContent.Contains("CodeGenerator.CodeGenerators").should_be_true();
+
+            file.fileContent.Contains("Entitas.CodeGeneration.CodeGenerator.PostProcessors").should_be_false();
+            file.fileContent.Contains("CodeGenerator.PostProcessors").should_be_true();
+        };
+    }
+}
