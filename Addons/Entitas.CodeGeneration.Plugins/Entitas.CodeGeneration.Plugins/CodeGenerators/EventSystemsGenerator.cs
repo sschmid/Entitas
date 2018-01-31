8e21ec626dbb8baf9b64532fc9d9449e1d9f6b61
Simon Schmid <mail@sschmid.com>
2018-01-31 20:56:32 +0100

    Added EventSystemsGenerator

---
+++ Addons/Entitas.CodeGeneration.Plugins/Entitas.CodeGeneration.Plugins/CodeGenerators/EventSystemsGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DesperateDevs.CodeGeneration;
+using DesperateDevs.Serialization;
+
+namespace Entitas.CodeGeneration.Plugins {
+
+    public class EventSystemsGenerator : ICodeGenerator, IConfigurable {
+
+        public string name { get { return "Event (Systems)"; } }
+        public int priority { get { return 0; } }
+        public bool runInDryMode { get { return true; } }
+
+        public Dictionary<string, string> defaultProperties { get { return _ignoreNamespacesConfig.defaultProperties; } }
+
+        readonly IgnoreNamespacesConfig _ignoreNamespacesConfig = new IgnoreNamespacesConfig();
+
+        const string SYSTEMS_TEMPLATE =
+            @"public sealed class EventSystems : Feature {
+
+    public EventSystems(Contexts contexts) {
+${systems}
+    }
+}
+";
+
+        const string SYSTEM_ADD_TEMPLATE = @"        Add(new ${ContextName}${ComponentName}EventSystem(contexts));";
+
+        public void Configure(Preferences preferences) {
+            _ignoreNamespacesConfig.Configure(preferences);
+        }
+
+        public CodeGenFile[] Generate(CodeGeneratorData[] data) {
+            var orderedEventData = data
+                .OfType<ComponentData>()
+                .Where(d => d.GetEventData() != null)
+                .OrderBy(d => d.GetEventData().priority)
+                .ThenBy(d => d.GetTypeName().ToComponentName(_ignoreNamespacesConfig.ignoreNamespaces))
+                .ToArray();
+
+            return new[] { generateEventSystems(orderedEventData) };
+        }
+
+        CodeGenFile generateEventSystems(ComponentData[] data) {
+            var systems = generateSystemList(data);
+
+            var fileContent = SYSTEMS_TEMPLATE
+                .Replace("${systems}", systems);
+
+            return new CodeGenFile(
+                "Events" + Path.DirectorySeparatorChar +
+                "EventSystems.cs",
+                fileContent,
+                GetType().FullName
+            );
+        }
+
+        string generateSystemList(ComponentData[] data) {
+            return string.Join("\n", data
+                .SelectMany(generateSystemListForData)
+                .ToArray());
+        }
+
+        string[] generateSystemListForData(ComponentData data) {
+            return data.GetContextNames()
+                .Select(contextName => generateAddSystem(contextName, data))
+                .ToArray();
+        }
+
+        string generateAddSystem(string contextName, ComponentData data) {
+            return SYSTEM_ADD_TEMPLATE
+                .Replace("${ContextName}", contextName)
+                .Replace("${ComponentName}", data.GetTypeName().ToComponentName(_ignoreNamespacesConfig.ignoreNamespaces));
+        }
+    }
+}
