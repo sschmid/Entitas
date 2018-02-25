40527f1134e3092316d90b7e3ae291eff22278b5
Simon Schmid <mail@sschmid.com>
2018-02-25 22:50:13 +0100

    Refactored EventSystemsGenerator

--- Addons/Entitas.CodeGeneration.Plugins/Entitas.CodeGeneration.Plugins/CodeGenerators/Events/EventSystemsGenerator.cs
+++ Addons/Entitas.CodeGeneration.Plugins/Entitas.CodeGeneration.Plugins/CodeGenerators/Events/EventSystemsGenerator.cs
@@ -1,53 +1,39 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using DesperateDevs.CodeGeneration;
-using DesperateDevs.Serialization;
 
 namespace Entitas.CodeGeneration.Plugins {
 
-    public class EventSystemsGenerator : ICodeGenerator, IConfigurable {
+    public class EventSystemsGenerator : AbstractComponentGenerator {
 
-        public string name { get { return "Event (Systems)"; } }
-        public int priority { get { return 0; } }
-        public bool runInDryMode { get { return true; } }
+        public override string name { get { return "Event (Systems)"; } }
 
-        public Dictionary<string, string> defaultProperties { get { return _ignoreNamespacesConfig.defaultProperties; } }
-
-        readonly IgnoreNamespacesConfig _ignoreNamespacesConfig = new IgnoreNamespacesConfig();
-
-        const string SYSTEMS_TEMPLATE =
+        const string TEMPLATE =
             @"public sealed class EventSystems : Feature {
 
     public EventSystems(Contexts contexts) {
-${systems}
+${systemsList}
     }
 }
 ";
 
-        const string SYSTEM_ADD_TEMPLATE = @"        Add(new ${OptionalContextName}${ComponentName}${EventType}EventSystem(contexts)); // priority: ${priority}";
+        const string SYSTEM_ADD_TEMPLATE = @"        Add(new ${Event}EventSystem(contexts)); // priority: ${priority}";
 
-        public void Configure(Preferences preferences) {
-            _ignoreNamespacesConfig.Configure(preferences);
-        }
-
-        public CodeGenFile[] Generate(CodeGeneratorData[] data) {
+        public override CodeGenFile[] Generate(CodeGeneratorData[] data) {
             var orderedEventData = data
                 .OfType<ComponentData>()
                 .Where(d => d.IsEvent())
                 .SelectMany(d => d.GetEventData().Select(eventData => new DataTuple { componentData = d, eventData = eventData }).ToArray())
                 .OrderBy(tuple => tuple.eventData.priority)
-                .ThenBy(tuple => tuple.componentData.GetTypeName().ToComponentName(_ignoreNamespacesConfig.ignoreNamespaces))
+                .ThenBy(tuple => tuple.componentData.ComponentName())
                 .ToArray();
 
-            return new[] { generateEventSystems(orderedEventData) };
+            return new[] { generate(orderedEventData) };
         }
 
-        CodeGenFile generateEventSystems(DataTuple[] data) {
-            var systems = generateSystemList(data);
+        CodeGenFile generate(DataTuple[] data) {
 
-            var fileContent = SYSTEMS_TEMPLATE
-                .Replace("${systems}", systems);
+            var fileContent = TEMPLATE.Replace("${systemsList}", generateSystemList(data));
 
             return new CodeGenFile(
                 "Events" + Path.DirectorySeparatorChar +
@@ -70,13 +56,10 @@ ${systems}
         }
 
         string generateAddSystem(string contextName, DataTuple data) {
-            var optionalContextName = data.componentData.GetContextNames().Length > 1 ? contextName : string.Empty;
-            var eventTypeSuffix = data.componentData.GetEventTypeSuffix(data.eventData);
             return SYSTEM_ADD_TEMPLATE
-                .Replace("${OptionalContextName}", optionalContextName)
-                .Replace("${ComponentName}", data.componentData.GetTypeName().ToComponentName(_ignoreNamespacesConfig.ignoreNamespaces))
-                .Replace("${EventType}", eventTypeSuffix)
-                .Replace("${priority}", data.eventData.priority.ToString());
+                .Replace(data.componentData, contextName, data.eventData)
+                .Replace("${priority}", data.eventData.priority.ToString())
+                .Replace("${Event}", data.componentData.Event(contextName, data.eventData));
         }
 
         struct DataTuple {
