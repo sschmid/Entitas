1c6b8c68b91e5d7c5903585f7aef965013433450
Simon Schmid <mail@sschmid.com>
2018-02-25 21:58:00 +0100

    Refactored EventEntityApiGenerator

--- Addons/Entitas.CodeGeneration.Plugins/Entitas.CodeGeneration.Plugins/CodeGenerators/CodeGeneratorExtentions.cs
+++ Addons/Entitas.CodeGeneration.Plugins/Entitas.CodeGeneration.Plugins/CodeGenerators/CodeGeneratorExtentions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using DesperateDevs.Utils;
+using Entitas.CodeGeneration.Attributes;
 
 namespace Entitas.CodeGeneration.Plugins {
 
@@ -17,29 +18,43 @@ namespace Entitas.CodeGeneration.Plugins {
 
         public static string Replace(this string template, ComponentData data, string contextName) {
             var componentName = data.ComponentName();
-
             return template
-
-                // Context
                 .Replace("${ContextType}", contextName.AddContextSuffix())
-
-                // Entity
                 .Replace("${EntityType}", contextName.AddEntitySuffix())
-
-                // Matcher
                 .Replace("${MatcherType}", contextName.AddMatcherSuffix())
-
-                // Component
                 .Replace("${ComponentType}", data.GetTypeName())
                 .Replace("${ComponentName}", componentName)
                 .Replace("${componentName}", componentName.LowercaseFirst())
-
-                // Unique component
                 .Replace("${prefixedComponentName}", data.GetUniquePrefix().LowercaseFirst() + componentName)
-                .Replace("${Context}", contextName)
-                .Replace("${componentName}", componentName.LowercaseFirst())
                 .Replace("${methodParameters}", getMethodParameters(data.GetMemberData()))
                 .Replace("${methodArgs}", getMethodArgs(data.GetMemberData()));
+        }
+
+        public static string Replace(this string template, ComponentData data, string contextName, EventData eventData) {
+            var eventListener = data.GetContextNames().Length > 1
+                ? contextName.LowercaseFirst() + data.ComponentName() + "Listener"
+                : data.ComponentName().LowercaseFirst() + "Listener";
+
+            return template
+                .Replace(data, contextName)
+                .Replace("${EventListener}", data.EventListener(contextName, eventData))
+                .Replace("${eventListener}", eventListener);
+        }
+
+        public static string EventListener(this ComponentData data, string contextName, EventData eventData) {
+            var optionalContextName = data.GetContextNames().Length > 1 ? contextName : string.Empty;
+            var eventTypeSuffix = eventData.eventType == EventType.Removed ? "Removed" : string.Empty;
+            return optionalContextName + data.ComponentName() + eventTypeSuffix + "Listener";
+        }
+
+        static string getEventMethodArgs(this ComponentData data, EventData eventData, string args) {
+            if (data.GetMemberData().Length == 0) {
+                return string.Empty;
+            }
+
+            return eventData.eventType == EventType.Removed
+                ? string.Empty
+                : args;
         }
 
         static string getMethodParameters(MemberData[] memberData) {
--- Addons/Entitas.CodeGeneration.Plugins/Entitas.CodeGeneration.Plugins/CodeGenerators/Events/EventEntityApiGenerator.cs
+++ Addons/Entitas.CodeGeneration.Plugins/Entitas.CodeGeneration.Plugins/CodeGenerators/Events/EventEntityApiGenerator.cs
@@ -1,87 +1,60 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using DesperateDevs.CodeGeneration;
-using DesperateDevs.Serialization;
 using DesperateDevs.Utils;
 
 namespace Entitas.CodeGeneration.Plugins {
 
-    public class EventEntityApiGenerator : ICodeGenerator, IConfigurable {
+    public class EventEntityApiGenerator : AbstractComponentGenerator {
 
-        public string name { get { return "Event (Entity API)"; } }
-        public int priority { get { return 0; } }
-        public bool runInDryMode { get { return true; } }
+        public override string name { get { return "Event (Entity API)"; } }
 
-        public Dictionary<string, string> defaultProperties { get { return _ignoreNamespacesConfig.defaultProperties; } }
+        const string TEMPLATE =
+            @"public partial class ${EntityType} {
 
-        readonly IgnoreNamespacesConfig _ignoreNamespacesConfig = new IgnoreNamespacesConfig();
-
-        const string COMPONENT_TEMPLATE =
-            @"public partial class ${ContextName}Entity {
-
-    public void Add${OptionalContextName}${ComponentName}${EventType}Listener(I${OptionalContextName}${ComponentName}${EventType}Listener value) {
-        var listeners = has${OptionalContextName}${ComponentName}${EventType}Listener
-            ? ${optionalContextName}${contextDependentComponentName}${EventType}Listener.value
-            : new System.Collections.Generic.List<I${OptionalContextName}${ComponentName}${EventType}Listener>();
+    public void Add${EventListener}(I${EventListener} value) {
+        var listeners = has${EventListener}
+            ? ${eventListener}.value
+            : new System.Collections.Generic.List<I${EventListener}>();
         listeners.Add(value);
-        Replace${OptionalContextName}${ComponentName}${EventType}Listener(listeners);
+        Replace${EventListener}(listeners);
     }
 
-    public void Remove${OptionalContextName}${ComponentName}Listener(I${OptionalContextName}${ComponentName}${EventType}Listener value, bool removeComponentWhenEmpty = true) {
-        var listeners = ${optionalContextName}${contextDependentComponentName}${EventType}Listener.value;
+    public void Remove${EventListener}(I${EventListener} value, bool removeComponentWhenEmpty = true) {
+        var listeners = ${eventListener}.value;
         listeners.Remove(value);
         if (removeComponentWhenEmpty && listeners.Count == 0) {
-            Remove${OptionalContextName}${ComponentName}${EventType}Listener();
+            Remove${EventListener}();
         } else {
-            Replace${OptionalContextName}${ComponentName}${EventType}Listener(listeners);
+            Replace${EventListener}(listeners);
         }
     }
 }
 ";
 
-        public void Configure(Preferences preferences) {
-            _ignoreNamespacesConfig.Configure(preferences);
-        }
-
-        public CodeGenFile[] Generate(CodeGeneratorData[] data) {
+        public override CodeGenFile[] Generate(CodeGeneratorData[] data) {
             return data
                 .OfType<ComponentData>()
                 .Where(d => d.IsEvent())
-                .SelectMany(generateComponents)
+                .SelectMany(generate)
                 .ToArray();
         }
 
-        CodeGenFile[] generateComponents(ComponentData data) {
+        CodeGenFile[] generate(ComponentData data) {
             return data.GetContextNames()
-                .SelectMany(contextName => generateComponent(contextName, data))
+                .SelectMany(contextName => generate(contextName, data))
                 .ToArray();
         }
 
-        CodeGenFile[] generateComponent(string contextName, ComponentData data) {
+        CodeGenFile[] generate(string contextName, ComponentData data) {
             return data.GetEventData()
-                .Select(eventData => {
-                    var optionalContextName = data.GetContextNames().Length > 1 ? contextName : string.Empty;
-                    var componentName = data.GetTypeName().ToComponentName(_ignoreNamespacesConfig.ignoreNamespaces);
-                    var eventTypeSuffix = data.GetEventTypeSuffix(eventData);
-
-                    var fileContent = COMPONENT_TEMPLATE
-                        .Replace("${ContextName}", contextName)
-                        .Replace("${OptionalContextName}", optionalContextName)
-                        .Replace("${optionalContextName}", optionalContextName == string.Empty ? string.Empty : optionalContextName.LowercaseFirst())
-                        .Replace("${ComponentName}", componentName)
-                        .Replace("${componentName}", componentName.LowercaseFirst())
-                        .Replace("${EventType}", eventTypeSuffix)
-                        .Replace("${contextDependentComponentName}", optionalContextName == string.Empty ? componentName.LowercaseFirst() : componentName);
-
-                    return new CodeGenFile(
-                        contextName + Path.DirectorySeparatorChar +
-                        "Components" + Path.DirectorySeparatorChar +
-                        contextName + optionalContextName + componentName + eventTypeSuffix + "Listener".AddComponentSuffix() + ".cs",
-                        fileContent,
-                        GetType().FullName
-                    );
-                }).ToArray();
+                .Select(eventData => new CodeGenFile(
+                    contextName + Path.DirectorySeparatorChar +
+                    "Components" + Path.DirectorySeparatorChar +
+                    contextName + data.EventListener(contextName, eventData).AddComponentSuffix() + ".cs",
+                    TEMPLATE.Replace(data, contextName, eventData),
+                    GetType().FullName
+                )).ToArray();
         }
     }
 }
