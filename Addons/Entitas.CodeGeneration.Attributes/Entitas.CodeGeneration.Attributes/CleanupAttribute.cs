0b69f748c5c54127a94078055a8a65ff9e6acb94
Simon Schmid <mail@sschmid.com>
2018-07-08 13:05:23 +0200

    Added CleanupAttribute

---
+++ Addons/Entitas.CodeGeneration.Attributes/Entitas.CodeGeneration.Attributes/CleanupAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Entitas.CodeGeneration.Attributes {
+
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum)]
+    public class CleanupAttribute : Attribute {
+
+        public readonly CleanupMode cleanupMode;
+
+        public CleanupAttribute(CleanupMode cleanupMode) {
+            this.cleanupMode = cleanupMode;
+        }
+    }
+
+    public enum CleanupMode {
+        RemoveComponent,
+        DestroyEntity
+    }
+}
