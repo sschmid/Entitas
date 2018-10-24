9b5d8413af3031ab53ee63cd688cb8297b4fa80e
Simon Schmid <mail@sschmid.com>
2018-10-24 21:46:57 +0200

    Added Entity Stress Test Scene

---
+++ Tests/Unity/VisualDebugging/Assets/Tests/Manual Tests/Entity Stress Test/EntityStressTestController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class EntityStressTestController : MonoBehaviour
+{
+    public int count;
+
+    Contexts _contexts;
+
+    void Start()
+    {
+        _contexts = Contexts.sharedInstance;
+
+        for (int i = 0; i < count; i++)
+        {
+            var e = _contexts.game.CreateEntity();
+            e.AddMyInt(i);
+            e.AddMyString(i.ToString());
+        }
+    }
+
+    void Update()
+    {
+        foreach (var e in _contexts.game.GetEntities())
+        {
+            e.ReplaceMyInt(e.myInt.myInt + 1);
+        }
+    }
+}
