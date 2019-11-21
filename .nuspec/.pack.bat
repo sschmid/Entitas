4d0370049b2be124e5cdc7924a53e4abfe591b79
Robin Avery <ribbanya@gmail.com>
2019-11-21 09:37:18 -0500

    Add NuGet specifications for almost all remaining assemblies, and a project to test dependencies.

--- .nuspec/.pack.bat
+++ .nuspec/.pack.bat
@@ -1,8 +1,15 @@
 @echo off
 
-rmdir .packages /s /q
-call msbuild /p:Configuration=Release
+rmdir ".packages" /s /q
+rmdir ".source" /s /q
+rmdir "bin" /s /q
+rmdir "obj" /s /q
+call nuget locals all -Clear
 for %%f in (*.nuspec) do (
     echo %%~nf
     call nuget pack %%f -OutputDirectory ".packages" -InstallPackageToOutputPath
 )
+call nuget init ".packages" ".source"
+call nuget restore ".nuspec.csproj"
+start /b msbuild ".nuspec.csproj" /p:Configuration=Debug /t:Build
+start /b msbuild ".nuspec.csproj" /p:Configuration=Release /t:Build
