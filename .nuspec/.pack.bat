3a19c666228189eaa0487449162ac092255ae0fd
Robin Avery <ribbanya@gmail.com>
2019-11-21 16:57:39 -0500

    Add remaining NuGet specifications.

--- .nuspec/.pack.bat
+++ .nuspec/.pack.bat
@@ -4,12 +4,23 @@ rmdir ".packages" /s /q
 rmdir ".source" /s /q
 rmdir "bin" /s /q
 rmdir "obj" /s /q
-call nuget locals all -Clear
-for %%f in (*.nuspec) do (
-    echo %%~nf
-    call nuget pack %%f -OutputDirectory ".packages" -InstallPackageToOutputPath
-)
-call nuget init ".packages" ".source"
-call nuget restore ".nuspec.csproj"
-start /b msbuild ".nuspec.csproj" /p:Configuration=Debug /t:Build
-start /b msbuild ".nuspec.csproj" /p:Configuration=Release /t:Build
+start /b nuget locals all -Clear
+
+:loop1
+    pslist nuget >nul 2>&1
+    if errorlevel 1 (goto continue1) else (goto loop1)
+
+:continue1
+    for %%f in (*.nuspec) do (
+        start /b nuget pack %%f -OutputDirectory ".packages"
+    )
+
+:loop2
+    pslist nuget >nul 2>&1
+    if errorlevel 1 (goto continue2) else (goto loop2)
+
+:continue2
+    call nuget init ".packages" ".source"
+    call nuget restore ".nuspec.csproj"
+    start /b msbuild ".nuspec.csproj" /p:Configuration=Debug /t:Build
+    start /b msbuild ".nuspec.csproj" /p:Configuration=Release /t:Build
