b13e006e4b5cdb9682036a798ea69572369d75ba
Simon Schmid <mail@sschmid.com>
2022-04-24 23:16:31 +0200

    Fix FeatureClassGenerator

--- Addons/Entitas.VisualDebugging.CodeGeneration.Plugins/Entitas.VisualDebugging.CodeGeneration.Plugins/FeatureClassGenerator.cs
+++ Addons/Entitas.VisualDebugging.CodeGeneration.Plugins/Entitas.VisualDebugging.CodeGeneration.Plugins/FeatureClassGenerator.cs
@@ -17,10 +17,9 @@ public class Feature : Entitas.VisualDebugging.Unity.DebugSystems {
     }
 
     public Feature() : base(true) {
-        var typeName = DesperateDevs.Utils.SerializationTypeExtension.ToCompilableString(GetType());
-        var shortType = DesperateDevs.Utils.SerializationTypeExtension.ShortTypeName(typeName);
-        var readableType = DesperateDevs.Utils.StringExtension.ToSpacedCamelCase(shortType);
-
+        var typeName = DesperateDevs.Extensions.TypeExtension.ToCompilableString(GetType());
+        var shortType = DesperateDevs.Extensions.TypeExtension.ShortTypeName(typeName);
+        var readableType = DesperateDevs.Extensions.StringExtension.ToSpacedCamelCase(shortType);
         initialize(readableType);
     }
 }
