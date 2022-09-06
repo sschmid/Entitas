e7b0dbcdb89c0089ea6fdfbe6f6de290c40be99a
Simon Schmid <mail@sschmid.com>
2022-09-06 11:22:57 +0200

    Migrate EntitasExceptionTests

---
+++ src/Entitas/tests/EntitasExceptionTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Entitas.Tests
+{
+    public class EntitasExceptionTests
+    {
+        [Fact]
+        public void CreatesExceptionWithHintSeparatedByNewLine()
+        {
+            const string msg = "Message";
+            const string hint = "Hint";
+            new EntitasException(msg, hint).Message
+                .Should().Be(msg + "\n" + hint);
+        }
+
+        [Fact]
+        public void IgnoresHintWhenNull()
+        {
+            const string msg = "Message";
+            var ex = new EntitasException(msg, null);
+            ex.Message.Should().Be(msg);
+        }
+    }
+}
