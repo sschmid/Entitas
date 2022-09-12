45f51978d5fd5fbd70fbb284bdbcb3a8d5e0dc85
Simon Schmid <mail@sschmid.com>
2022-09-12 11:34:46 +0200

    Migrate EntityIndexGeneratorTests

---
+++ src/Entitas.CodeGeneration/tests/EntityIndexGeneratorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Jenny;
+using Entitas.CodeGeneration.Plugins;
+using FluentAssertions;
+using Xunit;
+
+namespace Entitas.CodeGeneration.Tests
+{
+    public class EntityIndexGeneratorTests
+    {
+        [Fact]
+        public void DoesNotGenerateWhenNoIndexesSpecified()
+        {
+            new EntityIndexGenerator()
+                .Generate(Array.Empty<CodeGeneratorData>())
+                .Should().HaveCount(0);
+        }
+    }
+}
