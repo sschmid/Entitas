c29cf23201ffa56893d3fed3cb5ac31342991d2c
Simon Schmid <mail@sschmid.com>
2022-11-07 13:23:34 +0100

    Restructure project using tests folder

---
+++ tests/Entitas.Migration.Tests/MigrationUtilsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Entitas.Migration.Tests
+{
+    public class MigrationUtilsTests
+    {
+        static string FixturePath => $"{TestExtensions.GetProjectRoot()}/tests/Entitas.Migration.Tests.Fixtures/M0180";
+
+        [Fact]
+        public void GetsSourceFilesOnly()
+        {
+            var files = MigrationUtils.GetFiles(FixturePath);
+            files.Length.Should().Be(6);
+            files.Any(file => file.fileName == Path.Combine(FixturePath, "SourceFile.cs")).Should().BeTrue();
+            files.Any(file => file.fileName == Path.Combine(FixturePath, Path.Combine("SubFolder", "SourceFile2.cs"))).Should().BeTrue();
+            files.Any(file => file.fileName == Path.Combine(FixturePath, "RenderPositionSystem.cs")).Should().BeTrue();
+            files.Any(file => file.fileName == Path.Combine(FixturePath, "RenderRotationSystem.cs")).Should().BeTrue();
+            files.Any(file => file.fileName == Path.Combine(FixturePath, Path.Combine("SubFolder", "RenderSelectedSystem.cs"))).Should().BeTrue();
+            files.Any(file => file.fileName == Path.Combine(FixturePath, Path.Combine("SubFolder", "MoveSystem.cs"))).Should().BeTrue();
+        }
+    }
+}
