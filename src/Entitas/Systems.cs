7a97e8a2682bd5d27eacbfa040665e28bc619e46
Simon Schmid <mail@sschmid.com>
2022-11-23 17:29:48 +0100

    Cleanup Systems

--- src/Entitas/Systems.cs
+++ src/Entitas/Systems.cs
@@ -9,19 +9,10 @@ namespace Entitas
     /// you added them.
     public class Systems : IInitializeSystem, IExecuteSystem, ICleanupSystem, ITearDownSystem
     {
-        protected readonly List<IInitializeSystem> _initializeSystems;
-        protected readonly List<IExecuteSystem> _executeSystems;
-        protected readonly List<ICleanupSystem> _cleanupSystems;
-        protected readonly List<ITearDownSystem> _tearDownSystems;
-
-        /// Creates a new Systems instance.
-        public Systems()
-        {
-            _initializeSystems = new List<IInitializeSystem>();
-            _executeSystems = new List<IExecuteSystem>();
-            _cleanupSystems = new List<ICleanupSystem>();
-            _tearDownSystems = new List<ITearDownSystem>();
-        }
+        protected readonly List<IInitializeSystem> _initializeSystems = new List<IInitializeSystem>();
+        protected readonly List<IExecuteSystem> _executeSystems = new List<IExecuteSystem>();
+        protected readonly List<ICleanupSystem> _cleanupSystems = new List<ICleanupSystem>();
+        protected readonly List<ITearDownSystem> _tearDownSystems = new List<ITearDownSystem>();
 
         /// Adds the system instance to the systems list.
         public virtual Systems Add(ISystem system)
@@ -47,40 +38,39 @@ namespace Entitas
         /// nested Systems instances in the order you added them.
         public virtual void Initialize()
         {
-            for (var i = 0; i < _initializeSystems.Count; i++)
-                _initializeSystems[i].Initialize();
+            foreach (var system in _initializeSystems)
+                system.Initialize();
         }
 
         /// Calls Execute() on all IExecuteSystem and other
         /// nested Systems instances in the order you added them.
         public virtual void Execute()
         {
-            for (var i = 0; i < _executeSystems.Count; i++)
-                _executeSystems[i].Execute();
+            foreach (var system in _executeSystems)
+                system.Execute();
         }
 
         /// Calls Cleanup() on all ICleanupSystem and other
         /// nested Systems instances in the order you added them.
         public virtual void Cleanup()
         {
-            for (var i = 0; i < _cleanupSystems.Count; i++)
-                _cleanupSystems[i].Cleanup();
+            foreach (var system in _cleanupSystems)
+                system.Cleanup();
         }
 
         /// Calls TearDown() on all ITearDownSystem  and other
         /// nested Systems instances in the order you added them.
         public virtual void TearDown()
         {
-            for (var i = 0; i < _tearDownSystems.Count; i++)
-                _tearDownSystems[i].TearDown();
+            foreach (var system in _tearDownSystems)
+                system.TearDown();
         }
 
         /// Activates all ReactiveSystems in the systems list.
         public void ActivateReactiveSystems()
         {
-            for (var i = 0; i < _executeSystems.Count; i++)
+            foreach (var system in _executeSystems)
             {
-                var system = _executeSystems[i];
                 if (system is IReactiveSystem reactiveSystem)
                     reactiveSystem.Activate();
 
@@ -95,9 +85,8 @@ namespace Entitas
         /// want to reuse your existing system instances.
         public void DeactivateReactiveSystems()
         {
-            for (var i = 0; i < _executeSystems.Count; i++)
+            foreach (var system in _executeSystems)
             {
-                var system = _executeSystems[i];
                 if (system is IReactiveSystem reactiveSystem)
                     reactiveSystem.Deactivate();
 
@@ -109,9 +98,8 @@ namespace Entitas
         /// Clears all ReactiveSystems in the systems list.
         public void ClearReactiveSystems()
         {
-            for (var i = 0; i < _executeSystems.Count; i++)
+            foreach (var system in _executeSystems)
             {
-                var system = _executeSystems[i];
                 if (system is IReactiveSystem reactiveSystem)
                     reactiveSystem.Clear();
 
