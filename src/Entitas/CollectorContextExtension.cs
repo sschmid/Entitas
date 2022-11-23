1530dc5bbdd8aa8e758c2c52ac0fde0c6b9c52c7
Simon Schmid <mail@sschmid.com>
2022-11-23 10:54:29 +0100

    Cleanup CollectorContextExtension

--- src/Entitas/CollectorContextExtension.cs
+++ src/Entitas/CollectorContextExtension.cs
@@ -2,16 +2,12 @@
 {
     public static class CollectorContextExtension
     {
-        /// Creates a Collector.
-        public static ICollector<TEntity> CreateCollector<TEntity>(
-            this IContext<TEntity> context, IMatcher<TEntity> matcher) where TEntity : class, IEntity
-        {
-            return context.CreateCollector(new TriggerOnEvent<TEntity>(matcher, GroupEvent.Added));
-        }
+        public static ICollector<TEntity> CreateCollector<TEntity>(this IContext<TEntity> context, IMatcher<TEntity> matcher)
+            where TEntity : class, IEntity =>
+            context.CreateCollector(new TriggerOnEvent<TEntity>(matcher, GroupEvent.Added));
 
-        /// Creates a Collector.
-        public static ICollector<TEntity> CreateCollector<TEntity>(
-            this IContext<TEntity> context, params TriggerOnEvent<TEntity>[] triggers) where TEntity : class, IEntity
+        public static ICollector<TEntity> CreateCollector<TEntity>(this IContext<TEntity> context, params TriggerOnEvent<TEntity>[] triggers)
+            where TEntity : class, IEntity
         {
             var groups = new IGroup<TEntity>[triggers.Length];
             var groupEvents = new GroupEvent[triggers.Length];
