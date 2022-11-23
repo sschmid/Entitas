ef64fd0599506390518fa18b5e2d6fe6babbc10a
Simon Schmid <mail@sschmid.com>
2022-11-23 11:17:39 +0100

    Update Code Conventions in EntitasStringExtension

--- src/Entitas/EntitasStringExtension.cs
+++ src/Entitas/EntitasStringExtension.cs
@@ -2,39 +2,39 @@
 {
     public static class EntitasStringExtension
     {
-        public const string CONTEXT_SUFFIX = "Context";
-        public const string ENTITY_SUFFIX = "Entity";
-        public const string COMPONENT_SUFFIX = "Component";
-        public const string SYSTEM_SUFFIX = "System";
-        public const string MATCHER_SUFFIX = "Matcher";
-        public const string LISTENER_SUFFIX = "Listener";
+        public const string ContextSuffix = "Context";
+        public const string EntitySuffix = "Entity";
+        public const string ComponentSuffix = "Component";
+        public const string SystemSuffix = "System";
+        public const string MatcherSuffix = "Matcher";
+        public const string ListenerSuffix = "Listener";
 
-        public static string AddContextSuffix(this string str) => addSuffix(str, CONTEXT_SUFFIX);
-        public static string RemoveContextSuffix(this string str) => removeSuffix(str, CONTEXT_SUFFIX);
-        public static bool HasContextSuffix(this string str) => hasSuffix(str, CONTEXT_SUFFIX);
+        public static string AddContextSuffix(this string str) => AddSuffix(str, ContextSuffix);
+        public static string RemoveContextSuffix(this string str) => RemoveSuffix(str, ContextSuffix);
+        public static bool HasContextSuffix(this string str) => HasSuffix(str, ContextSuffix);
 
-        public static string AddEntitySuffix(this string str) => addSuffix(str, ENTITY_SUFFIX);
-        public static string RemoveEntitySuffix(this string str) => removeSuffix(str, ENTITY_SUFFIX);
-        public static bool HasEntitySuffix(this string str) => hasSuffix(str, ENTITY_SUFFIX);
+        public static string AddEntitySuffix(this string str) => AddSuffix(str, EntitySuffix);
+        public static string RemoveEntitySuffix(this string str) => RemoveSuffix(str, EntitySuffix);
+        public static bool HasEntitySuffix(this string str) => HasSuffix(str, EntitySuffix);
 
-        public static string AddComponentSuffix(this string str) => addSuffix(str, COMPONENT_SUFFIX);
-        public static string RemoveComponentSuffix(this string str) => removeSuffix(str, COMPONENT_SUFFIX);
-        public static bool HasComponentSuffix(this string str) => hasSuffix(str, COMPONENT_SUFFIX);
+        public static string AddComponentSuffix(this string str) => AddSuffix(str, ComponentSuffix);
+        public static string RemoveComponentSuffix(this string str) => RemoveSuffix(str, ComponentSuffix);
+        public static bool HasComponentSuffix(this string str) => HasSuffix(str, ComponentSuffix);
 
-        public static string AddSystemSuffix(this string str) => addSuffix(str, SYSTEM_SUFFIX);
-        public static string RemoveSystemSuffix(this string str) => removeSuffix(str, SYSTEM_SUFFIX);
-        public static bool HasSystemSuffix(this string str) => hasSuffix(str, SYSTEM_SUFFIX);
+        public static string AddSystemSuffix(this string str) => AddSuffix(str, SystemSuffix);
+        public static string RemoveSystemSuffix(this string str) => RemoveSuffix(str, SystemSuffix);
+        public static bool HasSystemSuffix(this string str) => HasSuffix(str, SystemSuffix);
 
-        public static string AddMatcherSuffix(this string str) => addSuffix(str, MATCHER_SUFFIX);
-        public static string RemoveMatcherSuffix(this string str) => removeSuffix(str, MATCHER_SUFFIX);
-        public static bool HasMatcherSuffix(this string str) => hasSuffix(str, MATCHER_SUFFIX);
+        public static string AddMatcherSuffix(this string str) => AddSuffix(str, MatcherSuffix);
+        public static string RemoveMatcherSuffix(this string str) => RemoveSuffix(str, MatcherSuffix);
+        public static bool HasMatcherSuffix(this string str) => HasSuffix(str, MatcherSuffix);
 
-        public static string AddListenerSuffix(this string str) => addSuffix(str, LISTENER_SUFFIX);
-        public static string RemoveListenerSuffix(this string str) => removeSuffix(str, LISTENER_SUFFIX);
-        public static bool HasListenerSuffix(this string str) => hasSuffix(str, LISTENER_SUFFIX);
+        public static string AddListenerSuffix(this string str) => AddSuffix(str, ListenerSuffix);
+        public static string RemoveListenerSuffix(this string str) => RemoveSuffix(str, ListenerSuffix);
+        public static bool HasListenerSuffix(this string str) => HasSuffix(str, ListenerSuffix);
 
-        static string addSuffix(string str, string suffix) => hasSuffix(str, suffix) ? str : str + suffix;
-        static string removeSuffix(string str, string suffix) => hasSuffix(str, suffix) ? str.Substring(0, str.Length - suffix.Length) : str;
-        static bool hasSuffix(string str, string suffix) => str.EndsWith(suffix, System.StringComparison.Ordinal);
+        static string AddSuffix(string str, string suffix) => HasSuffix(str, suffix) ? str : str + suffix;
+        static string RemoveSuffix(string str, string suffix) => HasSuffix(str, suffix) ? str.Substring(0, str.Length - suffix.Length) : str;
+        static bool HasSuffix(string str, string suffix) => str.EndsWith(suffix, System.StringComparison.Ordinal);
     }
 }
--- tests/Entitas.Tests/EntitasStringExtension.cs
+++ tests/Entitas.Tests/EntitasStringExtension.cs
@@ -10,23 +10,23 @@ namespace Entitas.Tests
         [Fact]
         public void DoesNotAddSuffixToStringEndingWithSuffix()
         {
-            AssertSameWord(Word + Entitas.EntitasStringExtension.CONTEXT_SUFFIX, (Word + Entitas.EntitasStringExtension.CONTEXT_SUFFIX).AddContextSuffix());
-            AssertSameWord(Word + Entitas.EntitasStringExtension.ENTITY_SUFFIX, (Word + Entitas.EntitasStringExtension.ENTITY_SUFFIX).AddEntitySuffix());
-            AssertSameWord(Word + Entitas.EntitasStringExtension.COMPONENT_SUFFIX, (Word + Entitas.EntitasStringExtension.COMPONENT_SUFFIX).AddComponentSuffix());
-            AssertSameWord(Word + Entitas.EntitasStringExtension.SYSTEM_SUFFIX, (Word + Entitas.EntitasStringExtension.SYSTEM_SUFFIX).AddSystemSuffix());
-            AssertSameWord(Word + Entitas.EntitasStringExtension.MATCHER_SUFFIX, (Word + Entitas.EntitasStringExtension.MATCHER_SUFFIX).AddMatcherSuffix());
-            AssertSameWord(Word + Entitas.EntitasStringExtension.LISTENER_SUFFIX, (Word + Entitas.EntitasStringExtension.LISTENER_SUFFIX).AddListenerSuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.ContextSuffix, (Word + Entitas.EntitasStringExtension.ContextSuffix).AddContextSuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.EntitySuffix, (Word + Entitas.EntitasStringExtension.EntitySuffix).AddEntitySuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.ComponentSuffix, (Word + Entitas.EntitasStringExtension.ComponentSuffix).AddComponentSuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.SystemSuffix, (Word + Entitas.EntitasStringExtension.SystemSuffix).AddSystemSuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.MatcherSuffix, (Word + Entitas.EntitasStringExtension.MatcherSuffix).AddMatcherSuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.ListenerSuffix, (Word + Entitas.EntitasStringExtension.ListenerSuffix).AddListenerSuffix());
         }
 
         [Fact]
         public void AddsSuffixToStringNotEndingWithSuffix()
         {
-            AssertSameWord(Word + Entitas.EntitasStringExtension.CONTEXT_SUFFIX, Word.AddContextSuffix());
-            AssertSameWord(Word + Entitas.EntitasStringExtension.ENTITY_SUFFIX, Word.AddEntitySuffix());
-            AssertSameWord(Word + Entitas.EntitasStringExtension.COMPONENT_SUFFIX, Word.AddComponentSuffix());
-            AssertSameWord(Word + Entitas.EntitasStringExtension.SYSTEM_SUFFIX, Word.AddSystemSuffix());
-            AssertSameWord(Word + Entitas.EntitasStringExtension.MATCHER_SUFFIX, Word.AddMatcherSuffix());
-            AssertSameWord(Word + Entitas.EntitasStringExtension.LISTENER_SUFFIX, Word.AddListenerSuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.ContextSuffix, Word.AddContextSuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.EntitySuffix, Word.AddEntitySuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.ComponentSuffix, Word.AddComponentSuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.SystemSuffix, Word.AddSystemSuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.MatcherSuffix, Word.AddMatcherSuffix());
+            AssertSameWord(Word + Entitas.EntitasStringExtension.ListenerSuffix, Word.AddListenerSuffix());
         }
 
         [Fact]
@@ -43,12 +43,12 @@ namespace Entitas.Tests
         [Fact]
         public void RemovesSuffixWhenEndingWithSuffix()
         {
-            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.CONTEXT_SUFFIX).RemoveContextSuffix());
-            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.ENTITY_SUFFIX).RemoveEntitySuffix());
-            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.COMPONENT_SUFFIX).RemoveComponentSuffix());
-            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.SYSTEM_SUFFIX).RemoveSystemSuffix());
-            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.MATCHER_SUFFIX).RemoveMatcherSuffix());
-            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.LISTENER_SUFFIX).RemoveListenerSuffix());
+            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.ContextSuffix).RemoveContextSuffix());
+            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.EntitySuffix).RemoveEntitySuffix());
+            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.ComponentSuffix).RemoveComponentSuffix());
+            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.SystemSuffix).RemoveSystemSuffix());
+            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.MatcherSuffix).RemoveMatcherSuffix());
+            AssertSameWord(Word, (Word + Entitas.EntitasStringExtension.ListenerSuffix).RemoveListenerSuffix());
         }
 
         [Fact]
@@ -65,12 +65,12 @@ namespace Entitas.Tests
         [Fact]
         public void HasSuffix()
         {
-            (Word + Entitas.EntitasStringExtension.CONTEXT_SUFFIX).HasContextSuffix().Should().BeTrue();
-            (Word + Entitas.EntitasStringExtension.ENTITY_SUFFIX).HasEntitySuffix().Should().BeTrue();
-            (Word + Entitas.EntitasStringExtension.COMPONENT_SUFFIX).HasComponentSuffix().Should().BeTrue();
-            (Word + Entitas.EntitasStringExtension.SYSTEM_SUFFIX).HasSystemSuffix().Should().BeTrue();
-            (Word + Entitas.EntitasStringExtension.MATCHER_SUFFIX).HasMatcherSuffix().Should().BeTrue();
-            (Word + Entitas.EntitasStringExtension.LISTENER_SUFFIX).HasListenerSuffix().Should().BeTrue();
+            (Word + Entitas.EntitasStringExtension.ContextSuffix).HasContextSuffix().Should().BeTrue();
+            (Word + Entitas.EntitasStringExtension.EntitySuffix).HasEntitySuffix().Should().BeTrue();
+            (Word + Entitas.EntitasStringExtension.ComponentSuffix).HasComponentSuffix().Should().BeTrue();
+            (Word + Entitas.EntitasStringExtension.SystemSuffix).HasSystemSuffix().Should().BeTrue();
+            (Word + Entitas.EntitasStringExtension.MatcherSuffix).HasMatcherSuffix().Should().BeTrue();
+            (Word + Entitas.EntitasStringExtension.ListenerSuffix).HasListenerSuffix().Should().BeTrue();
         }
 
         void AssertSameWord(string word1, string word2) => word1.Should().Be(word2);
