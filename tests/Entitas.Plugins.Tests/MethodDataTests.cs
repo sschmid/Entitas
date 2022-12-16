fffd0f7ba35cf6cca6b9694f2603e646ffade81c
Simon Schmid <mail@sschmid.com>
2022-12-16 19:54:18 +0100

    Add MethodDataTests

---
+++ tests/Entitas.Plugins.Tests/MethodDataTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Entitas.Plugins.Tests
+{
+    public class MethodDataTests
+    {
+        [Fact]
+        public void SetsFields()
+        {
+            var methodData = new MethodData("string", "Test", new[] {new MemberData("string", "value")});
+            methodData.ReturnType.Should().Be("string");
+            methodData.MethodName.Should().Be("Test");
+            methodData.Parameters.Should().HaveCount(1);
+            methodData.Parameters[0].Type.Should().Be("string");
+            methodData.Parameters[0].Name.Should().Be("value");
+        }
+    }
+}
