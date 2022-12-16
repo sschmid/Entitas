df5e937d6d0a14c9deeee49a576e4d06e67f0ffc
Simon Schmid <mail@sschmid.com>
2022-12-16 19:54:11 +0100

    Add MemberDataTests

---
+++ tests/Entitas.Plugins.Tests/MemberDataTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Entitas.Plugins.Tests
+{
+    public class MemberDataTests
+    {
+        [Fact]
+        public void SetsFields()
+        {
+            var memberData = new MemberData("string", "value");
+            memberData.Type.Should().Be("string");
+            memberData.Name.Should().Be("value");
+        }
+    }
+}
