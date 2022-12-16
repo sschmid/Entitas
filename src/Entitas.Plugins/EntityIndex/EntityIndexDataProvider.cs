76983d5ab4d68d9825ca71f534b5d4c01191bc56
Simon Schmid <mail@sschmid.com>
2022-12-16 21:30:06 +0100

    Refactor EntityIndexDataProvider

--- src/Entitas.Plugins/EntityIndex/EntityIndexDataProvider.cs
+++ src/Entitas.Plugins/EntityIndex/EntityIndexDataProvider.cs
@@ -25,11 +25,13 @@ namespace Entitas.Plugins
         readonly ProjectPathConfig _projectPathConfig = new ProjectPathConfig();
         readonly ContextConfig _contextConfig = new ContextConfig();
 
-        readonly INamedTypeSymbol[] _types;
+        readonly INamedTypeSymbol[] _symbols;
+
+        readonly string _componentInterface = typeof(IComponent).ToCompilableString();
 
         public EntityIndexDataProvider() : this(null) { }
 
-        public EntityIndexDataProvider(INamedTypeSymbol[] types) => _types = types;
+        public EntityIndexDataProvider(INamedTypeSymbol[] symbols) => _symbols = symbols;
 
         public void Configure(Preferences preferences)
         {
@@ -37,68 +39,70 @@ namespace Entitas.Plugins
             _contextConfig.Configure(preferences);
         }
 
+        ProjectParser GetProjectParser()
+        {
+            var key = typeof(ProjectParser).FullName;
+            if (!ObjectCache.TryGetValue(key, out var projectParser))
+            {
+                projectParser = new ProjectParser(_projectPathConfig.ProjectPath);
+                ObjectCache.Add(key, projectParser);
+            }
+
+            return (ProjectParser)projectParser;
+        }
+
         public CodeGeneratorData[] GetData()
         {
-            var types = _types ?? Jenny.Plugins.Roslyn.PluginUtil
-                .GetCachedProjectParser(ObjectCache, _projectPathConfig.ProjectPath)
-                .GetTypes();
+            var symbols = _symbols ?? GetProjectParser().GetTypes();
+            var concrete = symbols
+                .Where(symbol => !symbol.IsAbstract)
+                .ToArray();
 
-            var componentInterface = typeof(IComponent).ToCompilableString();
-
-            var entityIndexData = types
-                .Where(type => type.AllInterfaces.Any(i => i.ToCompilableString() == componentInterface))
-                .Where(type => !type.IsAbstract)
+            var entityIndexData = concrete
+                .Where(symbol => symbol.AllInterfaces.Any(i => i.ToCompilableString() == _componentInterface))
                 .ToDictionary(
-                    type => type,
-                    type => type.GetPublicMembers(true))
+                    symbol => symbol,
+                    symbol => symbol.GetPublicMembers(true))
                 .Where(kvp => kvp.Value.Any(symbol => symbol.GetAttribute<AbstractEntityIndexAttribute>(true) != null))
                 .SelectMany(kvp => CreateEntityIndexData(kvp.Key, kvp.Value));
 
-            var customEntityIndexData = types
-                .Where(type => !type.IsAbstract)
-                .Where(type => type.GetAttribute<CustomEntityIndexAttribute>() != null)
-                .Select(CreateCustomEntityIndexData);
+            var customEntityIndexData = concrete
+                .Where(symbol => symbol.GetAttribute<CustomEntityIndexAttribute>() != null)
+                .Select(symbol => CreateCustomEntityIndexData(symbol));
 
             // ReSharper disable once CoVariantArrayConversion
             return entityIndexData.Concat(customEntityIndexData).ToArray();
         }
 
-        IEnumerable<EntityIndexData> CreateEntityIndexData(INamedTypeSymbol type, ISymbol[] members)
+        IEnumerable<EntityIndexData> CreateEntityIndexData(INamedTypeSymbol symbol, ISymbol[] members) => members
+            .Select(member =>
+            {
+                var componentType = symbol.ToCompilableString();
+                var contexts = ComponentDataProvider.GetContexts(symbol, _contextConfig.Contexts);
+                if (contexts.Length == 0)
+                    contexts = new[] {_contextConfig.Contexts[0]};
+
+                return new EntityIndexData(
+                    type: GetEntityIndexType(member.GetAttribute<AbstractEntityIndexAttribute>(true)),
+                    name: componentType.ShortTypeName().RemoveComponentSuffix(),
+                    isCustom: false,
+                    customMethods: null,
+                    keyType: member.PublicMemberType().ToCompilableString(),
+                    componentType: componentType,
+                    memberName: member.Name,
+                    hasMultiple: members.Length > 1,
+                    contexts: contexts
+                );
+            });
+
+        EntityIndexData CreateCustomEntityIndexData(INamedTypeSymbol symbols)
         {
-            var indexes = members
-                .Where(member => member.GetAttribute<AbstractEntityIndexAttribute>(true) != null)
-                .ToArray();
-
-            return indexes
-                .Select(member =>
-                {
-                    var componentType = type.ToCompilableString();
-                    var contexts = ComponentDataProvider.GetContexts(type, _contextConfig.Contexts);
-                    if (contexts.Length == 0)
-                        contexts = new[] {_contextConfig.Contexts[0]};
-
-                    return new EntityIndexData(
-                        type: GetEntityIndexType(member.GetAttribute<AbstractEntityIndexAttribute>(true)),
-                        name: componentType.ShortTypeName().RemoveComponentSuffix(),
-                        isCustom: false,
-                        customMethods: null,
-                        keyType: member.PublicMemberType().ToCompilableString(),
-                        componentType: componentType,
-                        memberName: member.Name,
-                        hasMultiple: indexes.Length > 1,
-                        contexts: contexts
-                    );
-                });
-        }
-
-        EntityIndexData CreateCustomEntityIndexData(INamedTypeSymbol type)
-        {
-            var indexType = type.ToCompilableString();
+            var indexType = symbols.ToCompilableString();
             return new EntityIndexData(
                 indexType,
                 indexType.ShortTypeName(),
                 true,
-                type
+                symbols
                     .GetMembers()
                     .OfType<IMethodSymbol>()
                     .Where(method => method.DeclaredAccessibility == Accessibility.Public)
@@ -118,7 +122,7 @@ namespace Entitas.Plugins
                 false,
                 new[]
                 {
-                    ((INamedTypeSymbol)type.GetAttribute<CustomEntityIndexAttribute>().ConstructorArguments.First().Value)
+                    ((INamedTypeSymbol)symbols.GetAttribute<CustomEntityIndexAttribute>().ConstructorArguments.First().Value)
                     .ToCompilableString().ShortTypeName().RemoveContextSuffix()
                 }
             );
