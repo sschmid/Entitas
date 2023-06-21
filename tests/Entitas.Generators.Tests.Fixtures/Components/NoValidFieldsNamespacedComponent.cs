853baf6f4b6d211be5b05301d26699f19c9e02de
Simon Schmid <mail@sschmid.com>
2023-06-21 11:11:57 +0200

    Remove compiler warnings

--- tests/Entitas.Generators.Tests.Fixtures/Components/NoValidFieldsNamespacedComponent.cs
+++ tests/Entitas.Generators.Tests.Fixtures/Components/NoValidFieldsNamespacedComponent.cs
@@ -1,11 +1,15 @@
 using Entitas;
 
+#pragma warning disable CS0169
+#pragma warning disable CS0649
+
 namespace MyFeature
 {
     [MyApp.Main.Context]
     partial class NoValidFieldsNamespacedComponent : IComponent
     {
         public static string PublicStaticField;
+
         string _privateField;
         static string _privateStaticField;
     }
