a5020e8d55749fdbe834c65a4a1a42f12484aaaa
Simon Schmid <mail@sschmid.com>
2023-07-02 21:50:58 +0200

    Add Unsafe compatibility for Unity

--- gen/Entitas.Generators/Context/ContextGenerator.cs
+++ gen/Entitas.Generators/Context/ContextGenerator.cs
@@ -111,25 +111,42 @@ namespace Entitas.Generators
                     {
                         public static global::Entitas.IAllOfMatcher<Entity> AllOf(params ComponentIndex[] indices)
                         {
+                    #if NON_UNITY
                             var indexes = global::System.Runtime.CompilerServices.Unsafe.As<ComponentIndex[], int[]>(ref indices);
+                    #else
+                            var indexes = global::Unity.Collections.LowLevel.Unsafe.UnsafeUtility.As<ComponentIndex[], int[]>(ref indices);
+                    #endif
                             return global::Entitas.Matcher<Entity>.AllOf(indexes);
                         }
 
                         public static global::Entitas.IAnyOfMatcher<Entity> AnyOf(params ComponentIndex[] indices)
                         {
+                    #if NON_UNITY
                             var indexes = global::System.Runtime.CompilerServices.Unsafe.As<ComponentIndex[], int[]>(ref indices);
+                    #else
+                            var indexes = global::Unity.Collections.LowLevel.Unsafe.UnsafeUtility.As<ComponentIndex[], int[]>(ref indices);
+                    #endif
                             return global::Entitas.Matcher<Entity>.AnyOf(indexes);
                         }
 
                         public static global::Entitas.IAnyOfMatcher<Entity> AnyOf(this global::Entitas.IAllOfMatcher<Entity> matcher, params ComponentIndex[] indices)
                         {
+                    #if NON_UNITY
                             var indexes = global::System.Runtime.CompilerServices.Unsafe.As<ComponentIndex[], int[]>(ref indices);
+                    #else
+                            var indexes = global::Unity.Collections.LowLevel.Unsafe.UnsafeUtility.As<ComponentIndex[], int[]>(ref indices);
+                    #endif
                             return matcher.AnyOf(indexes);
                         }
 
                         public static global::Entitas.INoneOfMatcher<Entity> NoneOf(this global::Entitas.IAnyOfMatcher<Entity> matcher, params ComponentIndex[] indices)
                         {
+                            var indexes =
+                    #if NON_UNITY
                             var indexes = global::System.Runtime.CompilerServices.Unsafe.As<ComponentIndex[], int[]>(ref indices);
+                    #else
+                            var indexes = global::Unity.Collections.LowLevel.Unsafe.UnsafeUtility.As<ComponentIndex[], int[]>(ref indices);
+                    #endif
                             return matcher.NoneOf(indexes);
                         }
                     }
