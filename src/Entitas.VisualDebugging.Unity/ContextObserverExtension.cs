21b5da1810d754b7a9c7e1ccdff15352f230878d
Simon Schmid <mail@sschmid.com>
2023-07-10 19:32:39 +0200

    Add CreateContextObserver() extension method

--- src/Entitas.VisualDebugging.Unity/ContextObserverExtension.cs
+++ src/Entitas.VisualDebugging.Unity/ContextObserverExtension.cs
@@ -4,6 +4,14 @@ namespace Entitas.VisualDebugging.Unity
 {
     public static class ContextObserverExtension
     {
+#if (ENTITAS_DISABLE_VISUAL_DEBUGGING || !UNITY_EDITOR)
+        [System.Diagnostics.Conditional("false")]
+#endif
+        public static void CreateContextObserver(this IContext context)
+        {
+            Object.DontDestroyOnLoad(new ContextObserver(context).gameObject);
+        }
+
         public static ContextObserverBehaviour FindContextObserver(this IContext context)
         {
             var observers = Object.FindObjectsOfType<ContextObserverBehaviour>();
