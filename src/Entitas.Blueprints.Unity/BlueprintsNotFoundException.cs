7a451505f19ebbc381b244708d1f22a7acf8c268
Simon Schmid <mail@sschmid.com>
2023-07-20 12:44:16 +0200

    Delete Entitas.Blueprints

--- src/Entitas.Blueprints.Unity/BlueprintsNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Entitas.Blueprints.Unity
-{
-    public class BlueprintsNotFoundException : EntitasException
-    {
-        public BlueprintsNotFoundException(string blueprintName) : base(
-            $"'{blueprintName}' does not exist!",
-            "Did you update the Blueprints ScriptableObject?") { }
-    }
-}
